7694c443289d1587d9747db5b985ecba9d263b44
BooksCup <812809680@qq.com>
2019-02-01 10:43:45 +0800

    新增通过搜索添加好友的页面

--- app/src/main/java/com/bc/wechat/activity/NewFriendsMsgActivity.java
+++ app/src/main/java/com/bc/wechat/activity/NewFriendsMsgActivity.java
@@ -1,15 +1,41 @@
 package com.bc.wechat.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.bc.wechat.R;
 
 public class NewFriendsMsgActivity extends FragmentActivity {
+    TextView mAddTv;
+    TextView mSearchTv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_friends_msg);
+        initView();
+
+        mAddTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mSearchTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewFriendsMsgActivity.this, AddFriendsBySearchActivity.class));
+            }
+        });
+    }
+
+    private void initView() {
+        mAddTv = findViewById(R.id.tv_add);
+        mSearchTv = findViewById(R.id.tv_search);
     }
 }
