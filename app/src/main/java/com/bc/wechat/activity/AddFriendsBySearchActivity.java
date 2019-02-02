a20e1a3fe410afffa18a40290d5a22df7e2826d8
BooksCup <812809680@qq.com>
2019-02-02 11:09:14 +0800

    搜索好友页面新增返回

--- app/src/main/java/com/bc/wechat/activity/AddFriendsBySearchActivity.java
+++ app/src/main/java/com/bc/wechat/activity/AddFriendsBySearchActivity.java
@@ -3,6 +3,7 @@ package com.bc.wechat.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 import com.bc.wechat.R;
 
@@ -11,5 +12,9 @@ public class AddFriendsBySearchActivity extends FragmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friends_by_search);
+    }
+
+    public void back(View view) {
+        finish();
     }
 }
