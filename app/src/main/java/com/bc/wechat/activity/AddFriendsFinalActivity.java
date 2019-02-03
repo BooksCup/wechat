542106804df3f806f1f22587b72660e63a33ceef
BooksCup <812809680@qq.com>
2019-02-03 16:55:22 +0800

    验证申请光标移至最后

--- app/src/main/java/com/bc/wechat/activity/AddFriendsFinalActivity.java
+++ app/src/main/java/com/bc/wechat/activity/AddFriendsFinalActivity.java
@@ -3,6 +3,8 @@ package com.bc.wechat.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.text.Selection;
+import android.text.Spannable;
 import android.view.View;
 import android.widget.EditText;
 
@@ -26,6 +28,12 @@ public class AddFriendsFinalActivity extends FragmentActivity {
 
         String nickName = PreferencesUtil.getInstance().getUserNickName();
         mReasonEt.setText("我是" + nickName);
+        // 光标移至最后
+        CharSequence charSequence = mReasonEt.getText();
+        if (charSequence instanceof Spannable) {
+            Spannable spanText = (Spannable) charSequence;
+            Selection.setSelection(spanText, charSequence.length());
+        }
     }
 
     public void back(View view) {
