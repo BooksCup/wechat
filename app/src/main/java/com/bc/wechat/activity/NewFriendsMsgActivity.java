a50f4a29610f501538b7356eeddfc3e8457e08d5
BooksCup <812809680@qq.com>
2019-02-20 17:53:36 +0800

    修复一个传值BUG

--- app/src/main/java/com/bc/wechat/activity/NewFriendsMsgActivity.java
+++ app/src/main/java/com/bc/wechat/activity/NewFriendsMsgActivity.java
@@ -66,6 +66,7 @@ public class NewFriendsMsgActivity extends FragmentActivity {
                     // 如果已通过申请
                     // 进入用户详情页
                     startActivity(new Intent(NewFriendsMsgActivity.this, UserInfoActivity.class).
+                            putExtra("userId", friendApply.getFromUserId()).
                             putExtra("nickName", friendApply.getFromUserNickName()).
                             putExtra("avatar", friendApply.getFromUserAvatar()).
                             putExtra("sex", "2").
