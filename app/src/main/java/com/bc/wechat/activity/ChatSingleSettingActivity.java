42dbd34fa11dcf10db1b5c6353f441359ab4ec64
BooksCup <812809680@qq.com>
2019-03-04 11:01:18 +0800

    渲染单聊页面用户头像

--- app/src/main/java/com/bc/wechat/activity/ChatSingleSettingActivity.java
+++ app/src/main/java/com/bc/wechat/activity/ChatSingleSettingActivity.java
@@ -1,16 +1,20 @@
 package com.bc.wechat.activity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
 import com.bc.wechat.R;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 public class ChatSingleSettingActivity extends FragmentActivity {
 
     private TextView mNickNameTv;
+    private SimpleDraweeView mAvatarSdv;
 
     String userId;
     String userNickName;
@@ -29,8 +33,12 @@ public class ChatSingleSettingActivity extends FragmentActivity {
 
     private void initView() {
         mNickNameTv = findViewById(R.id.tv_nick_name);
+        mAvatarSdv = findViewById(R.id.sdv_avatar);
 
         mNickNameTv.setText(userNickName);
+        if (!TextUtils.isEmpty(userAvatar)) {
+            mAvatarSdv.setImageURI(Uri.parse(userAvatar));
+        }
     }
 
     public void back(View view) {
