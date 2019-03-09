64f0b75b704dbb0bd7de6bef0847202fe12ba2c1
BooksCup <812809680@qq.com>
2019-03-09 11:14:13 +0800

    修改签名页新增返回

--- app/src/main/java/com/bc/wechat/activity/UpdateSignActivity.java
+++ app/src/main/java/com/bc/wechat/activity/UpdateSignActivity.java
@@ -3,6 +3,7 @@ package com.bc.wechat.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 import com.bc.wechat.R;
 
@@ -12,5 +13,9 @@ public class UpdateSignActivity extends FragmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_sign);
+    }
+
+    public void back(View view) {
+        finish();
     }
 }
