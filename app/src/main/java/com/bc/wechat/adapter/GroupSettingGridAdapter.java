5870755e4023e9f4299bda94b2e7eda52bc423a7
BooksCup <812809680@qq.com>
2019-05-08 14:14:50 +0800

    群聊设置页面新增加减人按钮

--- app/src/main/java/com/bc/wechat/adapter/GroupSettingGridAdapter.java
+++ app/src/main/java/com/bc/wechat/adapter/GroupSettingGridAdapter.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.bc.wechat.R;
 import com.bc.wechat.entity.User;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.List;
 
@@ -23,7 +25,7 @@ public class GroupSettingGridAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return userList.size();
+        return userList.size() + 2;
     }
 
     @Override
@@ -39,6 +41,17 @@ public class GroupSettingGridAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         convertView = LayoutInflater.from(mContext).inflate(R.layout.item_chat_setting_gridview, null);
+        SimpleDraweeView mAvatarSdv = convertView.findViewById(R.id.sdv_avatar);
+        TextView mNickNameTv = convertView.findViewById(R.id.tv_username);
+        if (position == getCount() - 2) {
+            // 加人
+            mAvatarSdv.setImageResource(R.mipmap.jy_drltsz_btn_addperson);
+            mNickNameTv.setVisibility(View.INVISIBLE);
+        } else if (position == getCount() - 1) {
+            // 减人
+            mAvatarSdv.setImageResource(R.mipmap.icon_btn_deleteperson);
+            mNickNameTv.setVisibility(View.INVISIBLE);
+        }
         return convertView;
     }
 }
