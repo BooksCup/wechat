2f7b64cceeaac8237ff80b5cbe793ab145660f6a
BooksCup <812809680@qq.com>
2019-06-07 15:37:44 +0800

    新增修改群组edittext填充逻辑

--- app/src/main/java/com/bc/wechat/activity/UpdateGroupNameActivity.java
+++ app/src/main/java/com/bc/wechat/activity/UpdateGroupNameActivity.java
@@ -3,12 +3,21 @@ package com.bc.wechat.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.text.Selection;
+import android.text.Spannable;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
 import com.bc.wechat.R;
 
+import cn.jpush.im.android.api.JMessageClient;
+import cn.jpush.im.android.api.model.Conversation;
+import cn.jpush.im.android.api.model.GroupInfo;
+
 public class UpdateGroupNameActivity extends FragmentActivity {
     String groupId;
+    private EditText mGroupNameEt;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,7 +27,22 @@ public class UpdateGroupNameActivity extends FragmentActivity {
     }
 
     private void initView() {
+        mGroupNameEt = findViewById(R.id.et_group_name);
+
         groupId = getIntent().getStringExtra("groupId");
+        Conversation conversation = JMessageClient.getGroupConversation(Long.valueOf(groupId));
+        GroupInfo groupInfo = (GroupInfo) conversation.getTargetInfo();
+        if (TextUtils.isEmpty(groupInfo.getGroupDescription())) {
+            mGroupNameEt.setHint(groupInfo.getGroupName());
+        } else {
+            mGroupNameEt.setText(groupInfo.getGroupName());
+            // 光标移至最后
+            CharSequence charSequence = mGroupNameEt.getText();
+            if (charSequence instanceof Spannable) {
+                Spannable spanText = (Spannable) charSequence;
+                Selection.setSelection(spanText, charSequence.length());
+            }
+        }
     }
 
     public void back(View view) {
