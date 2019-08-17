d76b4cb85ab1c601b8aa9acbb79c172bf2ab4b9d
BooksCup <812809680@qq.com>
2019-08-17 16:44:39 +0800

    表单加入TextChange监听

--- app/src/main/java/com/bc/wechat/activity/RegisterActivity.java
+++ app/src/main/java/com/bc/wechat/activity/RegisterActivity.java
@@ -3,12 +3,15 @@ package com.bc.wechat.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
 import android.text.Html;
 import android.text.Selection;
 import android.text.Spannable;
+import android.text.TextWatcher;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,6 +26,8 @@ public class RegisterActivity extends FragmentActivity implements View.OnClickLi
 
     ImageView mHidePasswordIv;
     ImageView mShowPasswordIv;
+
+    Button mRegisterBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,14 +45,21 @@ public class RegisterActivity extends FragmentActivity implements View.OnClickLi
         mHidePasswordIv = findViewById(R.id.iv_password_hide);
         mShowPasswordIv = findViewById(R.id.iv_password_show);
 
+        mRegisterBtn = findViewById(R.id.btn_register);
+
         String agreement = "<font color=" + "\"" + "#AAAAAA" + "\">" + "点击上面的"
                 + "\"" + "注册" + "\"" + "按钮,即表示你同意" + "</font>" + "<u>"
                 + "<font color=" + "\"" + "#576B95" + "\">" + "《腾讯微信软件许可及服务协议》"
                 + "</font>" + "</u>";
         mAgreementTv.setText(Html.fromHtml(agreement));
 
+        mNickNameEt.addTextChangedListener(new TextChange());
+        mPhoneEt.addTextChangedListener(new TextChange());
+        mPasswordEt.addTextChangedListener(new TextChange());
+
         mHidePasswordIv.setOnClickListener(this);
         mShowPasswordIv.setOnClickListener(this);
+        mRegisterBtn.setOnClickListener(this);
     }
 
     public void back(View view) {
@@ -83,6 +95,33 @@ public class RegisterActivity extends FragmentActivity implements View.OnClickLi
                     Selection.setSelection(spanText, charSequence.length());
                 }
                 break;
+        }
+    }
+
+    class TextChange implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            boolean nickNameHasText = mNickNameEt.getText().toString().length() > 0;
+            boolean phoneHasText = mPhoneEt.getText().toString().length() > 0;
+            boolean passwordHasText = mPasswordEt.getText().toString().length() > 0;
+            if (nickNameHasText && phoneHasText && passwordHasText) {
+                mRegisterBtn.setTextColor(0xFFFFFFFF);
+                mRegisterBtn.setEnabled(true);
+            } else {
+                mRegisterBtn.setTextColor(0xFFD0EFC6);
+                mRegisterBtn.setEnabled(false);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
         }
     }
 }
