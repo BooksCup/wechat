730417608efa819f72858491d3768995dc57e210
BooksCup <812809680@qq.com>
2019-08-18 15:35:03 +0800

    完成注册调用服务器接口逻辑

--- app/src/main/java/com/bc/wechat/activity/RegisterActivity.java
+++ app/src/main/java/com/bc/wechat/activity/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.bc.wechat.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
@@ -15,10 +16,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.bc.wechat.R;
+import com.bc.wechat.cons.Constant;
+import com.bc.wechat.utils.VolleyUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends FragmentActivity implements View.OnClickListener {
+    private VolleyUtil volleyUtil;
     TextView mAgreementTv;
     EditText mNickNameEt;
     EditText mPhoneEt;
@@ -33,6 +43,7 @@ public class RegisterActivity extends FragmentActivity implements View.OnClickLi
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        volleyUtil = VolleyUtil.getInstance(this);
         initView();
     }
 
@@ -95,6 +106,13 @@ public class RegisterActivity extends FragmentActivity implements View.OnClickLi
                     Selection.setSelection(spanText, charSequence.length());
                 }
                 break;
+
+            case R.id.btn_register:
+                String nickName = mNickNameEt.getText().toString();
+                String phone = mPhoneEt.getText().toString();
+                String password = mPasswordEt.getText().toString();
+                register(nickName, phone, password);
+                break;
         }
     }
 
@@ -123,5 +141,32 @@ public class RegisterActivity extends FragmentActivity implements View.OnClickLi
         public void afterTextChanged(Editable editable) {
 
         }
+    }
+
+    private void register(String nickName, String phone, String password) {
+        String url = Constant.BASE_URL + "users";
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("nickName", nickName);
+        paramMap.put("phone", phone);
+        paramMap.put("password", password);
+
+        volleyUtil.httpPostRequest(url, paramMap, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                int errorCode = volleyError.networkResponse.statusCode;
+                switch (errorCode) {
+                    case 400:
+                        Toast.makeText(RegisterActivity.this,
+                                "账号或密码错误，请重新填写。", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/bc/wechat/cons/Constant.java
+++ app/src/main/java/com/bc/wechat/cons/Constant.java
@@ -4,7 +4,7 @@ package com.bc.wechat.cons;
  * 常量类
  */
 public class Constant {
-    public static final String BASE_URL = "http://192.168.0.138:8080/";
+    public static final String BASE_URL = "http://192.168.0.124:8080/";
 
     public static final String USER_SEX_MALE = "1";
     public static final String USER_SEX_FEMALE = "2";
