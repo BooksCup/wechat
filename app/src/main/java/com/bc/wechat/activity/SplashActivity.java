4c8fc2a6a9cf03c98f2d4c3bbdbf6702add55f7d
BooksCup <812809680@qq.com>
2019-08-23 09:40:36 +0800

    新增登录页

--- app/src/main/java/com/bc/wechat/activity/SplashActivity.java
+++ app/src/main/java/com/bc/wechat/activity/SplashActivity.java
@@ -1,18 +1,44 @@
 package com.bc.wechat.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 
 import com.bc.wechat.R;
 
 public class SplashActivity extends FragmentActivity {
+
+    private static final int sleepTime = 2000;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         final View view = View.inflate(this, R.layout.activity_splash, null);
         setContentView(view);
         super.onCreate(savedInstanceState);
 
+        AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
+        animation.setDuration(1500);
+        view.setAnimation(animation);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }).start();
     }
 }
