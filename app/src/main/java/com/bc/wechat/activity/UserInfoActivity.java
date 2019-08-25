5a307b395390ec531c77f82905f05151e3796662
BooksCup <812809680@qq.com>
2019-08-25 10:01:53 +0800

    完善用户详情页的头像

--- app/src/main/java/com/bc/wechat/activity/UserInfoActivity.java
+++ app/src/main/java/com/bc/wechat/activity/UserInfoActivity.java
@@ -2,16 +2,22 @@ package com.bc.wechat.activity;
 
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bc.wechat.R;
+import com.bc.wechat.cons.Constant;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 public class UserInfoActivity extends Activity {
 
     private TextView mNickNameTv;
+    private SimpleDraweeView mAvatarSdv;
+    private ImageView mSexIv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,12 +29,15 @@ public class UserInfoActivity extends Activity {
 
     private void initView() {
         mNickNameTv = findViewById(R.id.tv_name);
+        mAvatarSdv = findViewById(R.id.sdv_avatar);
+        mSexIv = findViewById(R.id.iv_sex);
 
         final String nickName = getIntent().getStringExtra("nickName");
         final String avatar = getIntent().getStringExtra("avatar");
         final String sex = getIntent().getStringExtra("sex");
 
         mNickNameTv.setText(nickName);
+        mAvatarSdv.setImageURI(Uri.parse(avatar));
     }
 
     public void back(View view) {
---
+++ app/src/main/java/com/bc/wechat/cons/Constant.java
@@ -0,0 +1,9 @@
+package com.bc.wechat.cons;
+
+/**
+ * 常量类
+ */
+public class Constant {
+    public static final String USER_SEX_MALE = "1";
+    public static final String USER_SEX_FEMALE = "2";
+}
