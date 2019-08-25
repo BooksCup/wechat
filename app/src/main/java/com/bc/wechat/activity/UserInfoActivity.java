7fd12ae27810d85e80266fd524f2fdbb4e235eed
BooksCup <812809680@qq.com>
2019-08-25 09:49:04 +0800

    新增initView

--- app/src/main/java/com/bc/wechat/activity/UserInfoActivity.java
+++ app/src/main/java/com/bc/wechat/activity/UserInfoActivity.java
@@ -4,13 +4,29 @@ package com.bc.wechat.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.bc.wechat.R;
 
 public class UserInfoActivity extends Activity {
+
+    private TextView mNickNameTv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        initView();
+    }
+
+    private void initView() {
+        mNickNameTv = findViewById(R.id.tv_name);
+
+        final String nickName = getIntent().getStringExtra("nickName");
+        final String avatar = getIntent().getStringExtra("avatar");
+        final String sex = getIntent().getStringExtra("sex");
+
+        mNickNameTv.setText(nickName);
     }
 }
