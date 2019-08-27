1c0876fa47dd9148027a9078df062c3bcb7b6af4
BooksCup <812809680@qq.com>
2019-08-27 10:06:40 +0800

    新增PasteEditText和AlertDialog

--- app/src/main/java/com/bc/wechat/activity/ChatActivity.java
+++ app/src/main/java/com/bc/wechat/activity/ChatActivity.java
@@ -1,9 +1,17 @@
 package com.bc.wechat.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.bc.wechat.R;
 
@@ -44,10 +52,45 @@ public class ChatActivity extends FragmentActivity implements View.OnClickListen
     public static final int CHATTYPE_GROUP = 2;
     public static final String COPY_IMAGE = "EASEMOBIMG";
 
+    private InputMethodManager manager;
+
+    private LinearLayout mMoreLl;
+    private LinearLayout mEmojiIconContainerLl;
+    private LinearLayout mBtnContainerLl;
+    private ImageView mEmoticonsNormalIv;
+    private ImageView mEmoticonsCheckedIv;
+
+    private RelativeLayout edittext_layout;
+    private View buttonSetModeVoice;
+    private View buttonSend;
+    private Button btnMore;
+    private View buttonPressToSpeak;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        initView();
+        setUpView();
+    }
+
+    private void initView() {
+        mMoreLl = findViewById(R.id.more);
+        mEmojiIconContainerLl = findViewById(R.id.ll_face_container);
+        mBtnContainerLl = findViewById(R.id.ll_btn_container);
+        mEmoticonsNormalIv = findViewById(R.id.iv_emoticons_normal);
+        mEmoticonsCheckedIv = findViewById(R.id.iv_emoticons_checked);
+        edittext_layout = (RelativeLayout) findViewById(R.id.edittext_layout);
+        buttonPressToSpeak = findViewById(R.id.btn_press_to_speak);
+        buttonSetModeVoice = findViewById(R.id.btn_set_mode_voice);
+        buttonSend = findViewById(R.id.btn_send);
+        btnMore = findViewById(R.id.btn_more);
+    }
+
+    private void setUpView() {
+        manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
     @Override
@@ -55,7 +98,67 @@ public class ChatActivity extends FragmentActivity implements View.OnClickListen
 
     }
 
+
     public void back(View view) {
         finish();
+    }
+
+    /**
+     * 显示键盘图标
+     *
+     * @param view
+     */
+    public void setModeKeyboard(View view) {
+        // mEditTextContent.setOnFocusChangeListener(new OnFocusChangeListener()
+        // {
+        // @Override
+        // public void onFocusChange(View v, boolean hasFocus) {
+        // if(hasFocus){
+        // getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        // }
+        // }
+        // });
+        edittext_layout.setVisibility(View.VISIBLE);
+        mMoreLl.setVisibility(View.GONE);
+        view.setVisibility(View.GONE);
+        buttonSetModeVoice.setVisibility(View.VISIBLE);
+        // mEditTextContent.setVisibility(View.VISIBLE);
+        // buttonSend.setVisibility(View.VISIBLE);
+        buttonPressToSpeak.setVisibility(View.GONE);
+
+    }
+
+    /**
+     * 显示或隐藏图标按钮页
+     *
+     * @param view
+     */
+    public void more(View view) {
+        if (mMoreLl.getVisibility() == View.GONE) {
+            hideKeyboard();
+            mMoreLl.setVisibility(View.VISIBLE);
+            mBtnContainerLl.setVisibility(View.VISIBLE);
+            mEmojiIconContainerLl.setVisibility(View.GONE);
+        } else {
+            if (mEmojiIconContainerLl.getVisibility() == View.VISIBLE) {
+                mEmojiIconContainerLl.setVisibility(View.GONE);
+                mBtnContainerLl.setVisibility(View.VISIBLE);
+                mEmoticonsNormalIv.setVisibility(View.VISIBLE);
+                mEmoticonsCheckedIv.setVisibility(View.INVISIBLE);
+            } else {
+                mMoreLl.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    /**
+     * 隐藏软键盘
+     */
+    private void hideKeyboard() {
+        if (getWindow().getAttributes().softInputMode != WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN) {
+            if (getCurrentFocus() != null)
+                manager.hideSoftInputFromWindow(getCurrentFocus()
+                        .getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 }
