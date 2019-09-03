a8ef887ff5617a6150a4355b10c6c38acea68125
BooksCup <812809680@qq.com>
2019-09-03 11:04:11 +0800

    新增设置页面

--- app/src/main/java/com/bc/wechat/fragment/ProfileFragment.java
+++ app/src/main/java/com/bc/wechat/fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.bc.wechat.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +8,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.bc.wechat.R;
+import com.bc.wechat.activity.SettingActivity;
 
+/**
+ * tab - "我"
+ */
 public class ProfileFragment extends Fragment {
+
+    private RelativeLayout mSettingRl;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mSettingRl = getView().findViewById(R.id.re_setting);
+        mSettingRl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
