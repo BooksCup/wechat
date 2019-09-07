b5da714a6268f5ab23bf49a263c1a7ff35337390
BooksCup <812809680@qq.com>
2019-09-07 14:12:40 +0800

    新增个人页面

---
+++ app/src/main/java/com/bc/wechat/activity/MyUserInfoActivity.java
@@ -0,0 +1,15 @@
+package com.bc.wechat.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+import com.bc.wechat.R;
+
+public class MyUserInfoActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myinfo);
+    }
+}
