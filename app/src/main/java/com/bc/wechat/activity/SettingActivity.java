e4ef6464e49c93dc4a4b05fc764f32a9696ba190
BooksCup <812809680@qq.com>
2019-09-07 13:40:33 +0800

    新增退出登录逻辑

--- app/src/main/java/com/bc/wechat/activity/SettingActivity.java
+++ app/src/main/java/com/bc/wechat/activity/SettingActivity.java
@@ -1,20 +1,51 @@
 package com.bc.wechat.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.bc.wechat.R;
+import com.bc.wechat.utils.PreferencesUtil;
 
-public class SettingActivity extends FragmentActivity {
+public class SettingActivity extends FragmentActivity implements View.OnClickListener {
+
+    private Button mLogoutBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        PreferencesUtil.getInstance().init(this);
+        initView();
+    }
+
+    private void initView() {
+        mLogoutBtn = findViewById(R.id.btn_logout);
+
+        mLogoutBtn.setOnClickListener(this);
     }
 
     public void back(View view) {
         finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_logout:
+                // 清除sharedpreferences中存储信息
+                PreferencesUtil.getInstance().setLogin(false);
+                PreferencesUtil.getInstance().setUserId("");
+
+                // 跳转至登录页
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+                break;
+        }
     }
 }
