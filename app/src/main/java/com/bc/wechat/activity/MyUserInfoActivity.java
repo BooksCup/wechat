e6114b9f9a693090ef9a19175f8d106a126e3ff0
BooksCup <812809680@qq.com>
2019-09-08 16:46:50 +0800

    新增修改昵称页面

--- app/src/main/java/com/bc/wechat/activity/MyUserInfoActivity.java
+++ app/src/main/java/com/bc/wechat/activity/MyUserInfoActivity.java
@@ -1,13 +1,18 @@
 package com.bc.wechat.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.bc.wechat.R;
 
 public class MyUserInfoActivity extends FragmentActivity implements View.OnClickListener {
+
+    private RelativeLayout mNickNameRl;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +21,9 @@ public class MyUserInfoActivity extends FragmentActivity implements View.OnClick
     }
 
     private void initView() {
+        mNickNameRl = findViewById(R.id.rl_nick_name);
 
+        mNickNameRl.setOnClickListener(this);
     }
 
     public void back(View view) {
@@ -25,6 +32,11 @@ public class MyUserInfoActivity extends FragmentActivity implements View.OnClick
 
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()) {
+            case R.id.rl_nick_name:
+                // 昵称
+                startActivity(new Intent(this, UpdateNickNameActivity.class));
+                break;
+        }
     }
 }
