5f6b2ca9fb262ccbd2790a230e0f007560bb874d
BooksCup <812809680@qq.com>
2019-09-12 12:20:19 +0800

    修改昵称edittext新增文本变更监听

--- app/src/main/java/com/bc/wechat/activity/UpdateNickNameActivity.java
+++ app/src/main/java/com/bc/wechat/activity/UpdateNickNameActivity.java
@@ -3,8 +3,10 @@ package com.bc.wechat.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
 import android.text.Selection;
 import android.text.Spannable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -41,9 +43,41 @@ public class UpdateNickNameActivity extends FragmentActivity {
             Spannable spanText = (Spannable) charSequence;
             Selection.setSelection(spanText, charSequence.length());
         }
+        mNickNameEt.addTextChangedListener(new TextChange());
     }
 
     public void back(View view) {
         finish();
+    }
+
+    class TextChange implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String newNickName = mNickNameEt.getText().toString();
+            String oldNickName = PreferencesUtil.getInstance().getUserNickName();
+            // 是否填写
+            boolean isNickNameHasText = newNickName.length() > 0;
+            // 是否修改
+            boolean isNickNameChanged = !oldNickName.equals(newNickName);
+
+            if (isNickNameHasText && isNickNameChanged) {
+                mSaveTv.setTextColor(0xFFFFFFFF);
+                mSaveTv.setEnabled(true);
+            } else {
+                mSaveTv.setTextColor(0xFFD0EFC6);
+                mSaveTv.setEnabled(false);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
     }
 }
