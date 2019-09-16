fa92387b4dabda947b1db115f1b51cc4e7d6ff5e
BooksCup <812809680@qq.com>
2019-09-16 10:14:48 +0800

    从SharePreference中取出userNickName，预置到修改昵称页

--- app/src/main/java/com/bc/wechat/activity/UpdateNickNameActivity.java
+++ app/src/main/java/com/bc/wechat/activity/UpdateNickNameActivity.java
@@ -6,9 +6,9 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bc.wechat.R;
+import com.bc.wechat.utils.PreferencesUtil;
 
 public class UpdateNickNameActivity extends FragmentActivity {
     private EditText mNickNameEt;
@@ -18,6 +18,7 @@ public class UpdateNickNameActivity extends FragmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_nick_name);
+        PreferencesUtil.getInstance().init(this);
         initView();
 
         mSaveTv.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +31,8 @@ public class UpdateNickNameActivity extends FragmentActivity {
     private void initView() {
         mNickNameEt = findViewById(R.id.et_nick);
         mSaveTv = findViewById(R.id.tv_save);
+
+        mNickNameEt.setText(PreferencesUtil.getInstance().getUserNickName());
     }
 
     public void back(View view) {
