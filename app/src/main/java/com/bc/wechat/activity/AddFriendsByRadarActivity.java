2da0f21583f5068ee129ebb853420bcffdb3784f
BooksCup <812809680@qq.com>
2019-09-28 10:32:33 +0800

    渲染通过雷达加好友页面的头像

--- app/src/main/java/com/bc/wechat/activity/AddFriendsByRadarActivity.java
+++ app/src/main/java/com/bc/wechat/activity/AddFriendsByRadarActivity.java
@@ -1,15 +1,25 @@
 package com.bc.wechat.activity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
 import com.bc.wechat.R;
+import com.bc.wechat.entity.User;
+import com.bc.wechat.utils.PreferencesUtil;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 public class AddFriendsByRadarActivity extends FragmentActivity {
+    private SimpleDraweeView mAvatarSdv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friends_by_radar);
+
+        mAvatarSdv = findViewById(R.id.sdv_avatar);
+        User user = PreferencesUtil.getInstance().getUser();
+        mAvatarSdv.setImageURI(Uri.parse(user.getUserAvatar()));
     }
 }
