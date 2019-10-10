d197a7e6884765a32f8a1fec8481856fec65355b
BooksCup <812809680@qq.com>
2019-10-10 10:05:57 +0800

    朋友圈页面调用服务器接口

--- app/src/main/java/com/bc/wechat/activity/FriendsCircleActivity.java
+++ app/src/main/java/com/bc/wechat/activity/FriendsCircleActivity.java
@@ -9,12 +9,20 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSONArray;
+import com.android.volley.NetworkError;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
 import com.bc.wechat.R;
 import com.bc.wechat.adapter.FriendsCircleAdapter;
+import com.bc.wechat.cons.Constant;
 import com.bc.wechat.entity.FriendsCircle;
 import com.bc.wechat.entity.User;
 import com.bc.wechat.utils.PreferencesUtil;
+import com.bc.wechat.utils.VolleyUtil;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.ArrayList;
@@ -24,20 +32,23 @@ public class FriendsCircleActivity extends FragmentActivity {
 
     private ListView listView;
     private User user;
+    private VolleyUtil volleyUtil;
+    private List<FriendsCircle> friendsCircleList = new ArrayList<>();
+    FriendsCircleAdapter mAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_circle);
         user = PreferencesUtil.getInstance().getUser();
+        volleyUtil = VolleyUtil.getInstance(this);
         listView = findViewById(R.id.ll_friends_circle);
         View headerView = LayoutInflater.from(this).inflate(R.layout.item_friends_circle_header, null);
 
-        List<FriendsCircle> list = new ArrayList<>();
-        list.add(new FriendsCircle());
-        list.add(new FriendsCircle());
-        FriendsCircleAdapter adapter = new FriendsCircleAdapter(list, this);
-        listView.setAdapter(adapter);
+        friendsCircleList.add(new FriendsCircle());
+        friendsCircleList.add(new FriendsCircle());
+        mAdapter = new FriendsCircleAdapter(friendsCircleList, this);
+        listView.setAdapter(mAdapter);
         listView.addHeaderView(headerView, null, false);
         listView.setHeaderDividersEnabled(false);
 
@@ -54,10 +65,36 @@ public class FriendsCircleActivity extends FragmentActivity {
 
         SimpleDraweeView mAvatarSdv = headerView.findViewById(R.id.sdv_avatar);
         mAvatarSdv.setImageURI(Uri.parse(user.getUserAvatar()));
+
+        getFriendsCircleList(user.getUserId());
     }
 
     public void back(View view) {
         finish();
+    }
+
+    private void getFriendsCircleList(String userId) {
+        String url = Constant.BASE_URL + "friendsCircle?userId=" + userId;
+
+        volleyUtil.httpGetRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                List<FriendsCircle> list = JSONArray.parseArray(response, FriendsCircle.class);
+                friendsCircleList.addAll(list);
+                mAdapter.notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                if (volleyError instanceof NetworkError) {
+                    Toast.makeText(FriendsCircleActivity.this, R.string.network_unavailable, Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (volleyError instanceof TimeoutError) {
+                    Toast.makeText(FriendsCircleActivity.this, R.string.network_time_out, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
     }
 
 }
