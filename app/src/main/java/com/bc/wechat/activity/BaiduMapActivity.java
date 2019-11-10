3115e0f7bf17bcb0b74e4454edfed83b09c1b284
BooksCup <812809680@qq.com>
2019-11-10 09:44:43 +0800

    接入百度地图

---
+++ app/src/main/java/com/bc/wechat/activity/BaiduMapActivity.java
@@ -0,0 +1,218 @@
+package com.bc.wechat.activity;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.widget.Toast;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BaiduMapOptions;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.utils.CoordinateConverter;
+import com.bc.wechat.R;
+
+public class BaiduMapActivity extends FragmentActivity {
+
+    MapView mMapView;
+    private BaiduMap mBaiduMap;
+    Button sendButton;
+    private LocationMode mCurrentMode;
+    ProgressDialog progressDialog;
+    BDLocation lastLocation;
+
+    public MyLocationListenner myListener = new MyLocationListenner();
+
+    // 定位相关
+    LocationClient mLocClient;
+
+    /**
+     * 构造广播监听类，监听 SDK key 验证以及网络异常广播
+     */
+    public class BaiduSDKReceiver extends BroadcastReceiver {
+        public void onReceive(Context context, Intent intent) {
+            String s = intent.getAction();
+            if (s.equals(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR)) {
+                Toast.makeText(BaiduMapActivity.this, "key 验证出错! 请在 AndroidManifest.xml 文件中检查 key 设置", Toast.LENGTH_SHORT).show();
+            } else if (s.equals(SDKInitializer.SDK_BROADCAST_ACTION_STRING_NETWORK_ERROR)) {
+                Toast.makeText(BaiduMapActivity.this, "网络出错", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private BaiduSDKReceiver mBaiduReceiver;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baidu_map);
+        requestWritePermission();
+
+        mMapView = findViewById(R.id.bmapView);
+        sendButton = findViewById(R.id.btn_location_send);
+        Intent intent = getIntent();
+        double latitude = intent.getDoubleExtra("latitude", 0);
+        mCurrentMode = LocationMode.NORMAL;
+        mBaiduMap = mMapView.getMap();
+        MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+        mBaiduMap.setMapStatus(msu);
+        mMapView.setLongClickable(true);
+        if (latitude == 0) {
+            mMapView = new MapView(this, new BaiduMapOptions());
+            mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
+                    mCurrentMode, true, null));
+            showMapWithLocationClient();
+        } else {
+            double longtitude = intent.getDoubleExtra("longitude", 0);
+            String address = intent.getStringExtra("address");
+            LatLng p = new LatLng(latitude, longtitude);
+            mMapView = new MapView(this,
+                    new BaiduMapOptions().mapStatus(new MapStatus.Builder()
+                            .target(p).build()));
+            showMap(latitude, longtitude, address);
+        }
+
+        // 注册 SDK 广播监听者
+        IntentFilter iFilter = new IntentFilter();
+        iFilter.addAction(SDKInitializer.SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR);
+        iFilter.addAction(SDKInitializer.SDK_BROADCAST_ACTION_STRING_NETWORK_ERROR);
+        mBaiduReceiver = new BaiduSDKReceiver();
+        registerReceiver(mBaiduReceiver, iFilter);
+    }
+
+    private void showMap(double latitude, double longtitude, String address) {
+        sendButton.setVisibility(View.GONE);
+        LatLng llA = new LatLng(latitude, longtitude);
+        CoordinateConverter converter = new CoordinateConverter();
+        converter.coord(llA);
+        converter.from(CoordinateConverter.CoordType.COMMON);
+        LatLng convertLatLng = converter.convert();
+        OverlayOptions ooA = new MarkerOptions().position(convertLatLng).icon(BitmapDescriptorFactory
+                .fromResource(R.mipmap.icon_marka))
+                .zIndex(4).draggable(true);
+        mBaiduMap.addOverlay(ooA);
+        MapStatusUpdate u = MapStatusUpdateFactory.newLatLngZoom(convertLatLng, 17.0f);
+        mBaiduMap.animateMapStatus(u);
+    }
+
+    private void showMapWithLocationClient() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setMessage("正在确定你的位置...");
+
+        progressDialog.setOnCancelListener(new OnCancelListener() {
+
+            public void onCancel(DialogInterface arg0) {
+                if (progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+                Log.d("map", "cancel retrieve location");
+                finish();
+            }
+        });
+
+        progressDialog.show();
+
+        mLocClient = new LocationClient(this);
+        mLocClient.registerLocationListener(myListener);
+
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);// 打开gps
+        // option.setCoorType("bd09ll"); //设置坐标类型
+        // Johnson change to use gcj02 coordination. chinese national standard
+        // so need to conver to bd09 everytime when draw on baidu map
+        option.setCoorType("bd09ll");
+        option.setScanSpan(30000);
+//        option.setAddrType("all");
+        mLocClient.setLocOption(option);
+        mLocClient.start();
+    }
+
+    /**
+     * 监听函数，有新位置的时候，格式化成字符串，输出到屏幕中
+     */
+    public class MyLocationListenner extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location == null) {
+                return;
+            }
+
+            Log.d("map", "On location change received:" + location);
+            Log.d("map", "addr:" + location.getAddrStr());
+            sendButton.setEnabled(true);
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+            }
+
+            if (lastLocation != null) {
+                if (lastLocation.getLatitude() == location.getLatitude() && lastLocation.getLongitude() == location.getLongitude()) {
+                    Log.d("map", "same location, skip refresh");
+                    // mMapView.refresh(); //need this refresh?
+                    return;
+                }
+            }
+            lastLocation = location;
+            mBaiduMap.clear();
+            LatLng llA = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+            CoordinateConverter converter = new CoordinateConverter();
+            converter.coord(llA);
+            converter.from(CoordinateConverter.CoordType.COMMON);
+            LatLng convertLatLng = converter.convert();
+            OverlayOptions ooA = new MarkerOptions().position(convertLatLng).icon(BitmapDescriptorFactory
+                    .fromResource(R.mipmap.icon_marka))
+                    .zIndex(4).draggable(true);
+            mBaiduMap.addOverlay(ooA);
+            MapStatusUpdate u = MapStatusUpdateFactory.newLatLngZoom(convertLatLng, 17.0f);
+            mBaiduMap.animateMapStatus(u);
+        }
+
+        public void onReceivePoi(BDLocation poiLocation) {
+            if (poiLocation == null) {
+                return;
+            }
+        }
+    }
+
+    // 运行时添加权限
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void requestWritePermission() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
+    }
+}
