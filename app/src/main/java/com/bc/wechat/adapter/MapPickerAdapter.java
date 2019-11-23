a123eb12866210a32acd910a37e404ec0dfdb31f
BooksCup <812809680@qq.com>
2019-11-23 14:57:09 +0800

    渲染地图选取适配器

--- app/src/main/java/com/bc/wechat/adapter/MapPickerAdapter.java
+++ app/src/main/java/com/bc/wechat/adapter/MapPickerAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.baidu.mapapi.search.core.PoiInfo;
 import com.bc.wechat.R;
@@ -39,11 +40,26 @@ public class MapPickerAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup viewGroup) {
-        convertView = LayoutInflater.from(mContext).inflate(R.layout.picker_item_place, null);
+        ViewHolder viewHolder;
+        if (null == convertView) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.picker_item_place, null);
+
+            viewHolder = new ViewHolder();
+            viewHolder.mNameTv = convertView.findViewById(R.id.tv_name);
+            viewHolder.mAddressTv = convertView.findViewById(R.id.tv_address);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        PoiInfo poiInfo = getItem(position);
+
+        viewHolder.mNameTv.setText(poiInfo.getName());
+        viewHolder.mAddressTv.setText(poiInfo.getAddress());
         return convertView;
     }
 
     class ViewHolder {
-
+        TextView mNameTv;
+        TextView mAddressTv;
     }
 }
