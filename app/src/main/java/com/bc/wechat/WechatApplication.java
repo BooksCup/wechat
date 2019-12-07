7652fff06de4f79f39380ea6421c4261ddae584f
BooksCup <812809680@qq.com>
2019-12-07 15:07:22 +0800

    定制上拉下拉控件的tips，优化新的朋友页面布局

--- app/src/main/java/com/bc/wechat/WechatApplication.java
+++ app/src/main/java/com/bc/wechat/WechatApplication.java
@@ -6,6 +6,7 @@ import com.baidu.mapapi.SDKInitializer;
 import com.bc.wechat.service.LocationService;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.orm.SugarContext;
+import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
 import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 
 import cn.jpush.android.api.JPushInterface;
@@ -29,13 +30,17 @@ public class WechatApplication extends Application {
         JMessageClient.init(this);
 
         // 上拉下拉控件
-        ClassicsHeader.REFRESH_HEADER_PULLING = "";//"下拉可以刷新";
-        ClassicsHeader.REFRESH_HEADER_RELEASE = "";//"释放立即刷新";
-        ClassicsHeader.REFRESH_HEADER_REFRESHING = "";//"正在刷新...";
-        ClassicsHeader.REFRESH_HEADER_RELEASE = "";//"释放立即刷新";
-        ClassicsHeader.REFRESH_HEADER_FINISH = "";//"刷新完成";
-        ClassicsHeader.REFRESH_HEADER_FAILED = "";//"刷新失败";
-        ClassicsHeader.REFRESH_HEADER_UPDATE = "";//"上次更新 M-d HH:mm";
+        ClassicsHeader.REFRESH_HEADER_PULLING = ""; //" 下拉可以刷新";
+        ClassicsHeader.REFRESH_HEADER_RELEASE = ""; // "释放立即刷新";
+        ClassicsHeader.REFRESH_HEADER_REFRESHING = ""; // "正在刷新...";
+        ClassicsHeader.REFRESH_HEADER_RELEASE = ""; // "释放立即刷新";
+        ClassicsHeader.REFRESH_HEADER_FINISH = ""; // "刷新完成";
+        ClassicsHeader.REFRESH_HEADER_FAILED = ""; // "刷新失败";
+        ClassicsHeader.REFRESH_HEADER_UPDATE = ""; // "上次更新 M-d HH:mm";
+
+        ClassicsFooter.REFRESH_FOOTER_REFRESHING = "正在加载"; // "正在加载..."
+        ClassicsFooter.REFRESH_FOOTER_FINISH = ""; // "加载完成"
+        ClassicsFooter.REFRESH_FOOTER_NOTHING = ""; // "全部加载完成"
 
         // 百度地图
         SDKInitializer.initialize(this);
