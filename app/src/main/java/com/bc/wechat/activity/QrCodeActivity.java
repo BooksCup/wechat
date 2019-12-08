97967186edb57c0db4abeea60a004f482e63e8c7
BooksCup <812809680@qq.com>
2019-12-08 14:31:07 +0800

    渲染二维码页面数据

--- app/src/main/java/com/bc/wechat/activity/QrCodeActivity.java
+++ app/src/main/java/com/bc/wechat/activity/QrCodeActivity.java
@@ -1,15 +1,50 @@
 package com.bc.wechat.activity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bc.wechat.R;
+import com.bc.wechat.cons.Constant;
+import com.bc.wechat.entity.User;
+import com.bc.wechat.utils.PreferencesUtil;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 public class QrCodeActivity extends BaseActivity {
+    private SimpleDraweeView mAvatarSdv;
+    private TextView mNickNameTv;
+    private ImageView mSexIv;
+
+    private User mUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_code);
+        mUser = PreferencesUtil.getInstance().getUser();
+        initView();
+    }
+
+    private void initView() {
+        mAvatarSdv = findViewById(R.id.sdv_avatar);
+        mNickNameTv = findViewById(R.id.tv_nick_name);
+        mSexIv = findViewById(R.id.iv_sex);
+
+        if (!TextUtils.isEmpty(mUser.getUserAvatar())) {
+            mAvatarSdv.setImageURI(Uri.parse(mUser.getUserAvatar()));
+        }
+        mNickNameTv.setText(mUser.getUserNickName());
+
+        if (Constant.USER_SEX_MALE.equals(mUser.getUserSex())) {
+            mSexIv.setImageResource(R.mipmap.ic_sex_male);
+        } else if (Constant.USER_SEX_FEMALE.equals(mUser.getUserSex())) {
+            mSexIv.setImageResource(R.mipmap.ic_sex_female);
+        } else {
+            mSexIv.setVisibility(View.GONE);
+        }
     }
 
     public void back(View view) {
