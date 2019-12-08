d4f720f8e611815d74dfa0f305ee40b0c010cf75
BooksCup <812809680@qq.com>
2019-12-08 14:19:00 +0800

    优化二维码页面命名

--- app/src/main/java/com/bc/wechat/activity/QrCodeActivity.java
+++ app/src/main/java/com/bc/wechat/activity/QrCodeActivity.java
@@ -1,6 +1,7 @@
 package com.bc.wechat.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.bc.wechat.R;
 
@@ -9,5 +10,9 @@ public class QrCodeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_code);
+    }
+
+    public void back(View view) {
+        finish();
     }
 }
