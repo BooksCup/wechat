15c073dcc15ae95c9aea9deb370c7571e8ac6ddb
BooksCup <812809680@qq.com>
2019-12-13 11:04:47 +0800

    新增两个扫一扫入口("发现"和"添加朋友")

--- app/src/main/java/com/bc/wechat/activity/AddFriendsActivity.java
+++ app/src/main/java/com/bc/wechat/activity/AddFriendsActivity.java
@@ -1,22 +1,38 @@
 package com.bc.wechat.activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
 import com.bc.wechat.R;
+import com.bc.wechat.entity.QrCodeContent;
 import com.bc.wechat.entity.User;
 import com.bc.wechat.utils.PreferencesUtil;
+import com.google.zxing.client.android.CaptureActivity2;
 
 public class AddFriendsActivity extends FragmentActivity implements View.OnClickListener {
+
+    private static final int SCAN_REQUEST_CODE = 100;
+    private static final int CAMERA_PERMISSION = 110;
 
     private RelativeLayout mSearchRl;
 
     private RelativeLayout mRadarRl;
+    private RelativeLayout mScanRl;
+
     private TextView mWxIdTv;
 
     private User mUser;
@@ -32,11 +48,14 @@ public class AddFriendsActivity extends FragmentActivity implements View.OnClick
     private void initView() {
         mSearchRl = findViewById(R.id.rl_search);
         mRadarRl = findViewById(R.id.rl_radar);
+        mScanRl = findViewById(R.id.rl_scan);
+
         mWxIdTv = findViewById(R.id.tv_wx_id);
 
         mWxIdTv.setText("我的微信号：" + mUser.getUserWxId());
         mSearchRl.setOnClickListener(this);
         mRadarRl.setOnClickListener(this);
+        mScanRl.setOnClickListener(this);
     }
 
     public void back(View view) {
@@ -53,6 +72,71 @@ public class AddFriendsActivity extends FragmentActivity implements View.OnClick
             case R.id.rl_radar:
                 startActivity(new Intent(this, AddFriendsByRadarActivity.class));
                 break;
+
+            case R.id.rl_scan:
+                if (Build.VERSION.SDK_INT > 22) {
+                    if (ContextCompat.checkSelfPermission(AddFriendsActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(AddFriendsActivity.this, new String[]{android.Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+                    } else {
+                        startScanActivity();
+                    }
+                } else {
+                    startScanActivity();
+                }
+                break;
+        }
+    }
+
+    private void startScanActivity() {
+        Intent intent = new Intent(AddFriendsActivity.this, CaptureActivity2.class);
+        intent.putExtra(CaptureActivity2.USE_DEFUALT_ISBN_ACTIVITY, true);
+        startActivityForResult(intent, SCAN_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startScanActivity();
+                } else {
+                    Toast.makeText(AddFriendsActivity.this, "请手动打开摄像头权限", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public int checkSelfPermission(String permission) {
+        return super.checkSelfPermission(permission);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SCAN_REQUEST_CODE) {
+                String isbn = data.getStringExtra("CaptureIsbn");
+                if (!TextUtils.isEmpty(isbn)) {
+                    if (isbn.contains("http")) {
+                        Intent intent = new Intent(this, WebViewActivity.class);
+                        intent.putExtra(WebViewActivity.RESULT, isbn);
+                        startActivity(intent);
+                    } else {
+                        try {
+                            QrCodeContent qrCodeContent = JSON.parseObject(isbn, QrCodeContent.class);
+                            if (QrCodeContent.QR_CODE_TYPE_USER.equals(qrCodeContent.getType())) {
+                                startActivity(new Intent(this, UserInfoActivity.class).
+                                        putExtra("userId", String.valueOf(qrCodeContent.getContentMap().get("userId"))));
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/bc/wechat/fragment/FindFragment.java
+++ app/src/main/java/com/bc/wechat/fragment/FindFragment.java
@@ -1,18 +1,35 @@
 package com.bc.wechat.fragment;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
 import com.bc.wechat.R;
 import com.bc.wechat.activity.FriendsCircleActivity;
+import com.bc.wechat.activity.UserInfoActivity;
+import com.bc.wechat.activity.WebViewActivity;
+import com.bc.wechat.entity.QrCodeContent;
+import com.google.zxing.client.android.CaptureActivity2;
 
 public class FindFragment extends Fragment {
+    private static final int SCAN_REQUEST_CODE = 100;
+    private static final int CAMERA_PERMISSION = 110;
+
     private RelativeLayout mFriendsCircleRl;
+    private RelativeLayout mScanRl;
+
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -24,6 +41,22 @@ public class FindFragment extends Fragment {
                 startActivity(new Intent(getActivity(), FriendsCircleActivity.class));
             }
         });
+
+        mScanRl = getView().findViewById(R.id.rl_scan);
+        mScanRl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT > 22) {
+                    if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{android.Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+                    } else {
+                        startScanActivity();
+                    }
+                } else {
+                    startScanActivity();
+                }
+            }
+        });
     }
 
     @Override
@@ -32,5 +65,52 @@ public class FindFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_find, container, false);
     }
 
+    private void startScanActivity() {
+        Intent intent = new Intent(getActivity(), CaptureActivity2.class);
+        intent.putExtra(CaptureActivity2.USE_DEFUALT_ISBN_ACTIVITY, true);
+        startActivityForResult(intent, SCAN_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startScanActivity();
+                } else {
+                    Toast.makeText(getActivity(), "请手动打开摄像头权限", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SCAN_REQUEST_CODE) {
+                String isbn = data.getStringExtra("CaptureIsbn");
+                if (!TextUtils.isEmpty(isbn)) {
+                    if (isbn.contains("http")) {
+                        Intent intent = new Intent(getActivity(), WebViewActivity.class);
+                        intent.putExtra(WebViewActivity.RESULT, isbn);
+                        startActivity(intent);
+                    } else {
+                        try {
+                            QrCodeContent qrCodeContent = JSON.parseObject(isbn, QrCodeContent.class);
+                            if (QrCodeContent.QR_CODE_TYPE_USER.equals(qrCodeContent.getType())) {
+                                startActivity(new Intent(getActivity(), UserInfoActivity.class).
+                                        putExtra("userId", String.valueOf(qrCodeContent.getContentMap().get("userId"))));
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
