160639159293ba4553e502bbfd24fcd615e65b06
BooksCup <812809680@qq.com>
2019-12-19 10:16:20 +0800

    退出登录清除本地朋友圈信息

--- app/src/main/java/com/bc/wechat/activity/SettingActivity.java
+++ app/src/main/java/com/bc/wechat/activity/SettingActivity.java
@@ -3,15 +3,15 @@ package com.bc.wechat.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.bc.wechat.R;
 import com.bc.wechat.entity.Friend;
+import com.bc.wechat.entity.FriendsCircle;
 import com.bc.wechat.utils.PreferencesUtil;
 
-public class SettingActivity extends FragmentActivity implements View.OnClickListener {
+public class SettingActivity extends BaseActivity implements View.OnClickListener {
 
     private Button mLogoutBtn;
 
@@ -41,7 +41,10 @@ public class SettingActivity extends FragmentActivity implements View.OnClickLis
                 PreferencesUtil.getInstance().setLogin(false);
                 PreferencesUtil.getInstance().setUser(null);
 
+                // 清除通讯录
                 Friend.deleteAll(Friend.class);
+                // 清除朋友圈
+                FriendsCircle.deleteAll(FriendsCircle.class);
 
                 // 跳转至登录页
                 Intent intent = new Intent(this, LoginActivity.class);
