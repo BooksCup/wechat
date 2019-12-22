0ebce9f74e4e63d24801ee07dc503bd42abd2ce6
BooksCup <812809680@qq.com>
2019-12-22 16:21:57 +0800

    新增默认头像的大图展示，取消不友好的Toast

--- app/src/main/java/com/bc/wechat/activity/BigImageActivity.java
+++ app/src/main/java/com/bc/wechat/activity/BigImageActivity.java
@@ -2,12 +2,13 @@ package com.bc.wechat.activity;
 
 import android.app.Activity;
 import android.graphics.drawable.Animatable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Toast;
 
 import com.bc.wechat.R;
+import com.facebook.common.util.UriUtil;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.backends.pipeline.PipelineDraweeControllerBuilder;
 import com.facebook.drawee.controller.BaseControllerListener;
@@ -36,24 +37,29 @@ public class BigImageActivity extends Activity {
 
     private void initData() {
         imgUrl = getIntent().getStringExtra("imgUrl");
+        PipelineDraweeControllerBuilder controller = Fresco.newDraweeControllerBuilder();
         if (!TextUtils.isEmpty(imgUrl)) {
-            PipelineDraweeControllerBuilder controller = Fresco.newDraweeControllerBuilder();
-            controller.setUri(imgUrl);//设置图片url
-            controller.setOldController(mPhotoDraweeView.getController());
-            controller.setControllerListener(new BaseControllerListener<ImageInfo>() {
-                @Override
-                public void onFinalImageSet(String id, ImageInfo imageInfo, Animatable animatable) {
-                    super.onFinalImageSet(id, imageInfo, animatable);
-                    if (imageInfo == null || mPhotoDraweeView == null) {
-                        return;
-                    }
-                    mPhotoDraweeView.update(imageInfo.getWidth(), imageInfo.getHeight());
-                }
-            });
-            mPhotoDraweeView.setController(controller.build());
+            // 设置图片url
+            controller.setUri(imgUrl);
         } else {
-            Toast.makeText(this, "图片获取失败", Toast.LENGTH_SHORT).show();
+            Uri uri = new Uri.Builder()
+                    .scheme(UriUtil.LOCAL_RESOURCE_SCHEME)
+                    .path(String.valueOf(R.mipmap.default_user_avatar))
+                    .build();
+            controller.setUri(uri);
         }
+        controller.setOldController(mPhotoDraweeView.getController());
+        controller.setControllerListener(new BaseControllerListener<ImageInfo>() {
+            @Override
+            public void onFinalImageSet(String id, ImageInfo imageInfo, Animatable animatable) {
+                super.onFinalImageSet(id, imageInfo, animatable);
+                if (imageInfo == null || mPhotoDraweeView == null) {
+                    return;
+                }
+                mPhotoDraweeView.update(imageInfo.getWidth(), imageInfo.getHeight());
+            }
+        });
+        mPhotoDraweeView.setController(controller.build());
     }
 
     private void initEvent() {
