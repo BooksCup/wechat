84090fe1a706cd43d58de598fdb36f99e21dcaec
BooksCup <812809680@qq.com>
2020-02-11 10:43:58 +0800

    temp commit

--- app/src/main/java/com/bc/wechat/activity/AddAddressActivity.java
+++ app/src/main/java/com/bc/wechat/activity/AddAddressActivity.java
@@ -7,23 +7,44 @@ import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.bc.wechat.R;
+import com.bc.wechat.cons.Constant;
+import com.bc.wechat.entity.User;
 import com.bc.wechat.utils.PreferencesUtil;
+import com.bc.wechat.utils.VolleyUtil;
+import com.bc.wechat.widget.LoadingDialog;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class AddAddressActivity extends FragmentActivity implements View.OnClickListener {
 
+    private EditText mNameEt;
+    private EditText mPhoneEt;
+    private EditText mAddressDetailEt;
     private EditText mAddressInfoEt;
+    private EditText mPostCodeEt;
+    private TextView mSaveTv;
+
+    private VolleyUtil mVolleyUtil;
+    private User mUser;
+
+    private LoadingDialog mDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_address);
+        mVolleyUtil = VolleyUtil.getInstance(this);
+        mUser = PreferencesUtil.getInstance().getUser();
+        mDialog = new LoadingDialog(AddAddressActivity.this);
         initView();
         PreferencesUtil.getInstance().init(this);
-        PreferencesUtil.getInstance().setPickedProvince("");
-        PreferencesUtil.getInstance().setPickedCity("");
-        PreferencesUtil.getInstance().setPickedDistrict("");
     }
 
     public void back(View view) {
@@ -31,9 +52,15 @@ public class AddAddressActivity extends FragmentActivity implements View.OnClick
     }
 
     private void initView() {
+        mNameEt = findViewById(R.id.et_name);
+        mPhoneEt = findViewById(R.id.et_phone);
         mAddressInfoEt = findViewById(R.id.et_address_info);
+        mAddressDetailEt = findViewById(R.id.et_address_detail);
+        mPostCodeEt = findViewById(R.id.et_post_code);
+        mSaveTv = findViewById(R.id.tv_save);
 
         mAddressInfoEt.setOnClickListener(this);
+        mSaveTv.setOnClickListener(this);
         PreferencesUtil.getInstance().setPickedProvince("");
         PreferencesUtil.getInstance().setPickedCity("");
         PreferencesUtil.getInstance().setPickedDistrict("");
@@ -44,6 +71,18 @@ public class AddAddressActivity extends FragmentActivity implements View.OnClick
         switch (view.getId()) {
             case R.id.et_address_info:
                 startActivity(new Intent(AddAddressActivity.this, PickProvinceActivity.class));
+                break;
+            case R.id.tv_save:
+                mDialog.setMessage(getString(R.string.saving));
+                mDialog.show();
+                String addressName = mNameEt.getText().toString();
+                String addressPhone = mPhoneEt.getText().toString();
+                String addressProvince = PreferencesUtil.getInstance().getPickedProvince();
+                String addressCity = PreferencesUtil.getInstance().getPickedCity();
+                String addressDistrict = PreferencesUtil.getInstance().getPickedDistrict();
+                String addressDetail = mAddressDetailEt.getText().toString();
+                String addressPostCode = mPostCodeEt.getText().toString();
+                addAddress(addressName, addressPhone, addressProvince, addressCity, addressDistrict, addressDetail, addressPostCode);
                 break;
         }
     }
@@ -63,5 +102,30 @@ public class AddAddressActivity extends FragmentActivity implements View.OnClick
                     .append(pickedDistrict);
             mAddressInfoEt.setText(addressInfoBuffer.toString());
         }
+    }
+
+    private void addAddress(String addressName, String addressPhone, String addressProvince,
+                            String addressCity, String addressDistrict, String addressDetail, String addressPostCode) {
+        String url = Constant.BASE_URL + "users/" + mUser.getUserId() + "/address";
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("addressName", addressName);
+        paramMap.put("addressPhone", addressPhone);
+        paramMap.put("addressProvince", addressProvince);
+        paramMap.put("addressCity", addressCity);
+        paramMap.put("addressDistrict", addressDistrict);
+        paramMap.put("addressDetail", addressDetail);
+        paramMap.put("addressPostCode", addressPostCode);
+
+        mVolleyUtil.httpPostRequest(url, paramMap, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                mDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                mDialog.dismiss();
+            }
+        });
     }
 }
