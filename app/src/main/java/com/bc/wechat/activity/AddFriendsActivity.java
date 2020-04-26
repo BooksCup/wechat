776041d0f13edb48820bd8589c4ba7c4c1c73f7c
BooksCup <812809680@qq.com>
2020-04-26 14:58:35 +0800

    新增添加朋友-扫一扫动态申请拍照的逻辑

--- app/src/main/java/com/bc/wechat/activity/AddFriendsActivity.java
+++ app/src/main/java/com/bc/wechat/activity/AddFriendsActivity.java
@@ -3,8 +3,10 @@ package com.bc.wechat.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -15,15 +17,22 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
 import com.bc.wechat.R;
 import com.bc.wechat.entity.QrCodeContent;
 import com.bc.wechat.entity.User;
 import com.bc.wechat.utils.PreferencesUtil;
+import com.bc.wechat.widget.ConfirmDialog;
 import com.google.zxing.client.android.CaptureActivity2;
 
+import java.util.ArrayList;
+
+/**
+ * 添加朋友
+ *
+ * @author zhou
+ */
 public class AddFriendsActivity extends FragmentActivity implements View.OnClickListener {
 
     private static final int SCAN_REQUEST_CODE = 100;
@@ -80,15 +89,8 @@ public class AddFriendsActivity extends FragmentActivity implements View.OnClick
                 break;
 
             case R.id.rl_scan:
-                if (Build.VERSION.SDK_INT > 22) {
-                    if (ContextCompat.checkSelfPermission(AddFriendsActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(AddFriendsActivity.this, new String[]{android.Manifest.permission.CAMERA}, CAMERA_PERMISSION);
-                    } else {
-                        startScanActivity();
-                    }
-                } else {
-                    startScanActivity();
-                }
+                String[] permissions = new String[]{"android.permission.CAMERA"};
+                requestPermissions(AddFriendsActivity.this, permissions, CAMERA_PERMISSION);
                 break;
 
             case R.id.ll_my_info:
@@ -105,18 +107,74 @@ public class AddFriendsActivity extends FragmentActivity implements View.OnClick
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case CAMERA_PERMISSION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startScanActivity();
-                } else {
-                    Toast.makeText(AddFriendsActivity.this, "请手动打开摄像头权限", Toast.LENGTH_SHORT).show();
-                }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean hasAllGranted = true;
+        // 判断是否拒绝  拒绝后要怎么处理 以及取消再次提示的处理
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                hasAllGranted = false;
                 break;
-            default:
-                break;
+            }
+        }
+        if (hasAllGranted) {
+            // 同意权限做的处理,开启服务提交通讯录
+            startScanActivity();
+        } else {
+            // 拒绝授权做的处理，弹出弹框提示用户授权
+            handleRejectPermission(this, permissions[0]);
         }
     }
+
+    /**
+     * 动态权限
+     */
+    public void requestPermissions(Activity activity, String[] permissions, int requestCode) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   //Android 6.0开始的动态权限，这里进行版本判断
+            ArrayList<String> mPermissionList = new ArrayList<>();
+            for (int i = 0; i < permissions.length; i++) {
+                if (ContextCompat.checkSelfPermission(activity, permissions[i])
+                        != PackageManager.PERMISSION_GRANTED) {
+                    mPermissionList.add(permissions[i]);
+                }
+            }
+            if (mPermissionList.isEmpty()) {
+                // 非初次进入App且已授权
+                startScanActivity();
+            } else {
+                // 请求权限方法
+                String[] requestPermissions = mPermissionList.toArray(new String[mPermissionList.size()]);
+                // 这个触发下面onRequestPermissionsResult这个回调
+                ActivityCompat.requestPermissions(this, requestPermissions, requestCode);
+            }
+        }
+    }
+
+    public void handleRejectPermission(final Activity context, String permission) {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(context, permission)) {
+            final ConfirmDialog mConfirmDialog = new ConfirmDialog(AddFriendsActivity.this, "权限申请",
+                    "在设置-应用-微信-权限中开启相机权限，以正常使用拍照、小视频、扫一扫等功能",
+                    "去设置", "取消", context.getColor(R.color.navy_blue));
+            mConfirmDialog.setOnDialogClickListener(new ConfirmDialog.OnDialogClickListener() {
+                @Override
+                public void onOkClick() {
+                    mConfirmDialog.dismiss();
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", context.getApplicationContext().getPackageName(), null);
+                    intent.setData(uri);
+                    context.startActivity(intent);
+                }
+
+                @Override
+                public void onCancelClick() {
+                    mConfirmDialog.dismiss();
+                }
+            });
+            // 点击空白处消失
+            mConfirmDialog.setCancelable(false);
+            mConfirmDialog.show();
+        }
+    }
+
 
     @Override
     public int checkSelfPermission(String permission) {
