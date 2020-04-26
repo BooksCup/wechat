57e58ca4f065fbeb94b2511c3e51f964a69657b8
BooksCup <812809680@qq.com>
2020-04-26 13:33:38 +0800

    新增发现-扫一扫动态申请拍照的逻辑

--- app/src/main/java/com/bc/wechat/fragment/FindFragment.java
+++ app/src/main/java/com/bc/wechat/fragment/FindFragment.java
@@ -3,9 +3,12 @@ package com.bc.wechat.fragment;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
@@ -13,7 +16,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
 import com.bc.wechat.R;
@@ -21,7 +23,10 @@ import com.bc.wechat.activity.FriendsCircleActivity;
 import com.bc.wechat.activity.UserInfoActivity;
 import com.bc.wechat.activity.WebViewActivity;
 import com.bc.wechat.entity.QrCodeContent;
+import com.bc.wechat.widget.ConfirmDialog;
 import com.google.zxing.client.android.CaptureActivity2;
+
+import java.util.ArrayList;
 
 public class FindFragment extends Fragment {
     private static final int SCAN_REQUEST_CODE = 100;
@@ -46,15 +51,8 @@ public class FindFragment extends Fragment {
         mScanRl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (Build.VERSION.SDK_INT > 22) {
-                    if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        requestPermissions(new String[]{android.Manifest.permission.CAMERA}, CAMERA_PERMISSION);
-                    } else {
-                        startScanActivity();
-                    }
-                } else {
-                    startScanActivity();
-                }
+                String[] permissions = new String[]{"android.permission.CAMERA"};
+                requestPerms(getActivity(), permissions, CAMERA_PERMISSION);
             }
         });
     }
@@ -63,27 +61,6 @@ public class FindFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_find, container, false);
-    }
-
-    private void startScanActivity() {
-        Intent intent = new Intent(getActivity(), CaptureActivity2.class);
-        intent.putExtra(CaptureActivity2.USE_DEFUALT_ISBN_ACTIVITY, true);
-        startActivityForResult(intent, SCAN_REQUEST_CODE);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case CAMERA_PERMISSION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startScanActivity();
-                } else {
-                    Toast.makeText(getActivity(), "请手动打开摄像头权限", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
-                break;
-        }
     }
 
     @Override
@@ -111,6 +88,85 @@ public class FindFragment extends Fragment {
                 }
             }
         }
+    }
+
+    /**
+     * 动态权限
+     */
+    public void requestPerms(Activity activity, String[] permissions, int requestCode) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   //Android 6.0开始的动态权限，这里进行版本判断
+            ArrayList<String> mPermissionList = new ArrayList<>();
+            for (int i = 0; i < permissions.length; i++) {
+                if (ContextCompat.checkSelfPermission(activity, permissions[i])
+                        != PackageManager.PERMISSION_GRANTED) {
+                    mPermissionList.add(permissions[i]);
+                }
+            }
+            if (mPermissionList.isEmpty()) {
+                // 非初次进入App且已授权
+                startScanActivity();
+            } else {
+                // 请求权限方法
+                String[] requestPermissions = mPermissionList.toArray(new String[mPermissionList.size()]);
+                // 这个触发下面onRequestPermissionsResult这个回调
+                requestPermissions(requestPermissions, requestCode);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean hasAllGranted = true;
+        // 判断是否拒绝  拒绝后要怎么处理 以及取消再次提示的处理
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                hasAllGranted = false;
+                break;
+            }
+        }
+        if (hasAllGranted) {
+            // 同意权限做的处理,开启服务提交通讯录
+            startScanActivity();
+        } else {
+            // 拒绝授权做的处理，弹出弹框提示用户授权
+            handleRejectPermission(getActivity(), permissions[0]);
+        }
+    }
+
+    public void handleRejectPermission(final Activity context, String permission) {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(context, permission)) {
+            final ConfirmDialog mConfirmDialog = new ConfirmDialog(getActivity(), "权限申请",
+                    "在设置-应用-微信-权限中开启相机权限，以正常使用拍照、小视频、扫一扫等功能",
+                    "去设置", "取消", context.getColor(R.color.navy_blue));
+            mConfirmDialog.setOnDialogClickListener(new ConfirmDialog.OnDialogClickListener() {
+                @Override
+                public void onOkClick() {
+                    mConfirmDialog.dismiss();
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", context.getApplicationContext().getPackageName(), null);
+                    intent.setData(uri);
+                    context.startActivity(intent);
+                }
+
+                @Override
+                public void onCancelClick() {
+                    mConfirmDialog.dismiss();
+                }
+            });
+            // 点击空白处消失
+            mConfirmDialog.setCancelable(false);
+            mConfirmDialog.show();
+        }
+    }
+
+    /**
+     * 进入扫一扫页面
+     */
+    private void startScanActivity() {
+        Intent intent = new Intent(getActivity(), CaptureActivity2.class);
+        intent.putExtra(CaptureActivity2.USE_DEFUALT_ISBN_ACTIVITY, true);
+        startActivityForResult(intent, SCAN_REQUEST_CODE);
     }
 
 }
