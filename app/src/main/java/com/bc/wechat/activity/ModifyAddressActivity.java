f3f50c45d9932e3f335a4c76aa2c802770d88937
BooksCup <812809680@qq.com>
2020-05-12 14:34:39 +0800

    完善修改地址逻辑

--- app/src/main/java/com/bc/wechat/activity/ModifyAddressActivity.java
+++ app/src/main/java/com/bc/wechat/activity/ModifyAddressActivity.java
@@ -1,14 +1,26 @@
 package com.bc.wechat.activity;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,13 +31,16 @@ import com.android.volley.VolleyError;
 import com.bc.wechat.R;
 import com.bc.wechat.cons.Constant;
 import com.bc.wechat.dao.AddressDao;
+import com.bc.wechat.dao.AreaDao;
 import com.bc.wechat.entity.Address;
+import com.bc.wechat.entity.Area;
 import com.bc.wechat.entity.User;
 import com.bc.wechat.utils.PreferencesUtil;
 import com.bc.wechat.utils.VolleyUtil;
 import com.bc.wechat.widget.ConfirmDialog;
 import com.bc.wechat.widget.LoadingDialog;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,6 +50,10 @@ import java.util.Map;
  * @author zhou
  */
 public class ModifyAddressActivity extends FragmentActivity implements View.OnClickListener {
+
+    private static final int REQUEST_CODE_CONTACTS = 0;
+    private static final int REQUEST_CODE_LOCATION = 1;
+
     private TextView mTitleTv;
 
     private EditText mNameEt;
@@ -50,6 +69,10 @@ public class ModifyAddressActivity extends FragmentActivity implements View.OnCl
     private LoadingDialog mDialog;
     private Address mAddress;
     private AddressDao mAddressDao;
+    private AreaDao mAreaDao;
+
+    private ImageView mAddressBookIv;
+    private ImageView mLocationIv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,6 +82,7 @@ public class ModifyAddressActivity extends FragmentActivity implements View.OnCl
         mUser = PreferencesUtil.getInstance().getUser();
         mDialog = new LoadingDialog(ModifyAddressActivity.this);
         mAddressDao = new AddressDao();
+        mAreaDao = new AreaDao();
         initView();
         PreferencesUtil.getInstance().init(this);
     }
@@ -81,9 +105,9 @@ public class ModifyAddressActivity extends FragmentActivity implements View.OnCl
                 addressInfoModifyFlag ||
                 addressDetailModifyFlag ||
                 addressPostCodeModifyFlag) {
-            final ConfirmDialog confirmDialog = new ConfirmDialog(ModifyAddressActivity.this, "提示",
-                    "是否放弃修改地址信息？",
-                    "确定", getString(R.string.cancel));
+            final ConfirmDialog confirmDialog = new ConfirmDialog(ModifyAddressActivity.this, getString(R.string.tips),
+                    getString(R.string.modify_address_abandon_tips),
+                    getString(R.string.ok), getString(R.string.cancel), getColor(R.color.navy_blue));
             confirmDialog.setOnDialogClickListener(new ConfirmDialog.OnDialogClickListener() {
                 @Override
                 public void onOkClick() {
@@ -106,7 +130,7 @@ public class ModifyAddressActivity extends FragmentActivity implements View.OnCl
 
     private void initView() {
         mTitleTv = findViewById(R.id.tv_title);
-        mTitleTv.setText("修改地址");
+        mTitleTv.setText(getString(R.string.modify_address));
 
         mAddress = (Address) getIntent().getSerializableExtra("address");
 
@@ -116,6 +140,9 @@ public class ModifyAddressActivity extends FragmentActivity implements View.OnCl
         mAddressDetailEt = findViewById(R.id.et_address_detail);
         mPostCodeEt = findViewById(R.id.et_post_code);
         mSaveTv = findViewById(R.id.tv_save);
+
+        mAddressBookIv = findViewById(R.id.iv_address_book);
+        mLocationIv = findViewById(R.id.iv_location);
 
         mNameEt.setText(mAddress.getAddressName());
         mPhoneEt.setText(mAddress.getAddressPhone());
@@ -131,6 +158,9 @@ public class ModifyAddressActivity extends FragmentActivity implements View.OnCl
 
         mAddressInfoEt.setOnClickListener(this);
         mSaveTv.setOnClickListener(this);
+        mAddressBookIv.setOnClickListener(this);
+        mLocationIv.setOnClickListener(this);
+
         PreferencesUtil.getInstance().setPickedProvince("");
         PreferencesUtil.getInstance().setPickedCity("");
         PreferencesUtil.getInstance().setPickedDistrict("");
@@ -185,6 +215,7 @@ public class ModifyAddressActivity extends FragmentActivity implements View.OnCl
 
     @Override
     public void onClick(View view) {
+        String[] permissions;
         switch (view.getId()) {
             case R.id.et_address_info:
                 startActivity(new Intent(ModifyAddressActivity.this, PickProvinceActivity.class));
@@ -202,6 +233,72 @@ public class ModifyAddressActivity extends FragmentActivity implements View.OnCl
                 modifyAddress(mAddress.getAddressId(), addressName, addressPhone, addressProvince,
                         addressCity, addressDistrict, addressDetail, addressPostCode);
                 break;
+
+            case R.id.iv_address_book:
+                permissions = new String[]{"android.permission.READ_CONTACTS"};
+                requestPermissions(ModifyAddressActivity.this, permissions, REQUEST_CODE_CONTACTS);
+                break;
+            case R.id.iv_location:
+                permissions = new String[]{"android.permission.ACCESS_FINE_LOCATION"};
+                requestPermissions(ModifyAddressActivity.this, permissions, REQUEST_CODE_LOCATION);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE_CONTACTS:
+                    if (data != null) {
+                        Uri uri = data.getData();
+                        String name = null;
+                        String phoneNumber = null;
+                        ContentResolver contentResolver = getContentResolver();
+                        Cursor cursor = contentResolver.query(uri,
+                                null, null, null, null);
+                        while (cursor.moveToNext()) {
+                            name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                            phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        }
+                        cursor.close();
+                        phoneNumber = phoneNumber.replaceAll(" ", "");
+                        if (!TextUtils.isEmpty(name)) {
+                            mNameEt.setText(name);
+                        }
+                        if (!TextUtils.isEmpty(phoneNumber)) {
+                            mPhoneEt.setText(phoneNumber);
+                        }
+                    }
+                    break;
+                case REQUEST_CODE_LOCATION:
+                    // 获取省市区以及详细地址信息
+                    String province = data.getStringExtra("province");
+                    String city = data.getStringExtra("city");
+                    String district = data.getStringExtra("district");
+                    String addressDetail = data.getStringExtra("addressDetail");
+                    PreferencesUtil.getInstance().setPickedProvince(province);
+                    PreferencesUtil.getInstance().setPickedCity(city);
+                    PreferencesUtil.getInstance().setPickedDistrict(district);
+
+                    StringBuffer addressInfoBuffer = new StringBuffer();
+                    addressInfoBuffer.append(province).append(" ")
+                            .append(city).append(" ")
+                            .append(district);
+                    mAddressInfoEt.setText(addressInfoBuffer.toString());
+                    mAddressDetailEt.setText(addressDetail);
+
+                    Area districtArea = mAreaDao.getDistrictByCityNameAndDistrictName(city, district);
+                    if (TextUtils.isEmpty(districtArea.getPostCode())) {
+                        mPostCodeEt.setText(Constant.DEFAULT_POST_CODE);
+                    } else {
+                        mPostCodeEt.setText(districtArea.getPostCode());
+                    }
+
+                    break;
+            }
         }
     }
 
@@ -267,5 +364,131 @@ public class ModifyAddressActivity extends FragmentActivity implements View.OnCl
                 }
             }
         });
+    }
+
+    /**
+     * 动态权限
+     */
+    public void requestPermissions(Activity activity, String[] permissions, int requestCode) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   //Android 6.0开始的动态权限，这里进行版本判断
+            ArrayList<String> mPermissionList = new ArrayList<>();
+            for (int i = 0; i < permissions.length; i++) {
+                if (ContextCompat.checkSelfPermission(activity, permissions[i])
+                        != PackageManager.PERMISSION_GRANTED) {
+                    mPermissionList.add(permissions[i]);
+                }
+            }
+            if (mPermissionList.isEmpty()) {
+                // 非初次进入App且已授权
+                switch (requestCode) {
+                    case REQUEST_CODE_CONTACTS:
+                        showContacts();
+                        break;
+                    case REQUEST_CODE_LOCATION:
+                        showMapPicker();
+                        break;
+                }
+
+            } else {
+                // 请求权限方法
+                String[] requestPermissions = mPermissionList.toArray(new String[mPermissionList.size()]);
+                // 这个触发下面onRequestPermissionsResult这个回调
+                ActivityCompat.requestPermissions(activity, requestPermissions, requestCode);
+            }
+        }
+    }
+
+    /**
+     * requestPermissions的回调
+     * 一个或多个权限请求结果回调
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean hasAllGranted = true;
+        // 判断是否拒绝  拒绝后要怎么处理 以及取消再次提示的处理
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                hasAllGranted = false;
+                break;
+            }
+        }
+        if (hasAllGranted) {
+            switch (requestCode) {
+                case REQUEST_CODE_CONTACTS:
+                    // 同意通讯录权限,开启通讯录服务
+                    showContacts();
+                    break;
+                case REQUEST_CODE_LOCATION:
+                    // 同意定位权限,进入地图选择器
+                    showMapPicker();
+                    break;
+            }
+        } else {
+            // 拒绝授权做的处理，弹出弹框提示用户授权
+            handleRejectPermission(ModifyAddressActivity.this, permissions[0], requestCode);
+        }
+    }
+
+    public void handleRejectPermission(final Activity context, String permission, int requestCode) {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(context, permission)) {
+            String content = "";
+            switch (requestCode) {
+                case REQUEST_CODE_CONTACTS:
+                    content = getString(R.string.request_permission_contacts);
+                    break;
+                case REQUEST_CODE_LOCATION:
+                    content = getString(R.string.request_permission_location);
+                    break;
+            }
+            final ConfirmDialog mConfirmDialog = new ConfirmDialog(ModifyAddressActivity.this, getString(R.string.request_permission),
+                    content,
+                    getString(R.string.go_setting), getString(R.string.cancel), getColor(R.color.navy_blue));
+            mConfirmDialog.setOnDialogClickListener(new ConfirmDialog.OnDialogClickListener() {
+                @Override
+                public void onOkClick() {
+                    mConfirmDialog.dismiss();
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", context.getApplicationContext().getPackageName(), null);
+                    intent.setData(uri);
+                    context.startActivity(intent);
+                }
+
+                @Override
+                public void onCancelClick() {
+                    mConfirmDialog.dismiss();
+                }
+            });
+            // 点击空白处消失
+            mConfirmDialog.setCancelable(false);
+            mConfirmDialog.show();
+        }
+    }
+
+    /**
+     * 跳转到通讯录
+     */
+    private void showContacts() {
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.PICK");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.setType("vnd.android.cursor.dir/phone_v2");
+        startActivityForResult(intent, REQUEST_CODE_CONTACTS);
+    }
+
+    /**
+     * 进入地图选择页面
+     */
+    private void showMapPicker() {
+        Intent intent = new Intent(ModifyAddressActivity.this, MapPickerActivity.class);
+        // 是否发送定位
+        // true:发送定位  false:无发送按钮，显示定位
+        intent.putExtra("sendLocation", true);
+
+        // 定位类型
+        // 获取省市区信息
+        intent.putExtra("locationType", Constant.LOCATION_TYPE_AREA);
+        startActivityForResult(intent, REQUEST_CODE_LOCATION);
     }
 }
