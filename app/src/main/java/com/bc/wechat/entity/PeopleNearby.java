68467236d3553f32f5d8cec92db50ee492dd13f6
BooksCup <812809680@qq.com>
2020-05-17 10:15:17 +0800

    渲染附近的人列表

---
+++ app/src/main/java/com/bc/wechat/entity/PeopleNearby.java
@@ -0,0 +1,87 @@
+package com.bc.wechat.entity;
+
+public class PeopleNearby {
+    private String id;
+    private String userId;
+    private String longitude;
+    private String latitude;
+    private String region;
+
+    // 用户信息
+    private String userNickName;
+    private String userAvatar;
+    private String userSex;
+    private String userSign;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getUserNickName() {
+        return userNickName;
+    }
+
+    public void setUserNickName(String userNickName) {
+        this.userNickName = userNickName;
+    }
+
+    public String getUserAvatar() {
+        return userAvatar;
+    }
+
+    public void setUserAvatar(String userAvatar) {
+        this.userAvatar = userAvatar;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getUserSign() {
+        return userSign;
+    }
+
+    public void setUserSign(String userSign) {
+        this.userSign = userSign;
+    }
+}
