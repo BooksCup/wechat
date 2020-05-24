abc99f836520a43bdc0d09169d23384de8a41175
BooksCup <812809680@qq.com>
2020-05-24 17:18:56 +0800

    重做页面(tab-"发现")

--- app/src/main/java/com/bc/wechat/fragment/DiscoverFragment.java
+++ app/src/main/java/com/bc/wechat/fragment/DiscoverFragment.java
@@ -74,7 +74,7 @@ public class DiscoverFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_find, container, false);
+        return inflater.inflate(R.layout.fragment_discover, container, false);
     }
 
     @Override
