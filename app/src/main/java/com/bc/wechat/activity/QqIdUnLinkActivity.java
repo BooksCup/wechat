52db46aecd5b27945874fdc3fe657d1ae9823f8c
BooksCup <812809680@qq.com>
2020-06-13 11:31:29 +0800

    实现解绑QQ号的逻辑

--- app/src/main/java/com/bc/wechat/activity/QqIdUnLinkActivity.java
+++ app/src/main/java/com/bc/wechat/activity/QqIdUnLinkActivity.java
@@ -3,9 +3,18 @@ package com.bc.wechat.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.bc.wechat.R;
+import com.bc.wechat.cons.Constant;
+import com.bc.wechat.entity.User;
+import com.bc.wechat.utils.PreferencesUtil;
 import com.bc.wechat.utils.StatusBarUtil;
+import com.bc.wechat.utils.VolleyUtil;
+import com.bc.wechat.widget.LoadingDialog;
+import com.bc.wechat.widget.NoTitleConfirmDialog;
 
 /**
  * 解绑QQ号
@@ -13,6 +22,13 @@ import com.bc.wechat.utils.StatusBarUtil;
  * @author zhou
  */
 public class QqIdUnLinkActivity extends BaseActivity {
+
+    private Button mUnLinkQqIdBtn;
+
+    private VolleyUtil mVolleyUtil;
+    private User mUser;
+    private LoadingDialog mDialog;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +36,68 @@ public class QqIdUnLinkActivity extends BaseActivity {
 
         initStatusBar();
         StatusBarUtil.setStatusBarColor(QqIdUnLinkActivity.this, R.color.bottom_text_color_normal);
+
+        mVolleyUtil = VolleyUtil.getInstance(this);
+        mUser = PreferencesUtil.getInstance().getUser();
+        mDialog = new LoadingDialog(this);
+
+        mUnLinkQqIdBtn = findViewById(R.id.btn_unlink_qq);
+        mUnLinkQqIdBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final NoTitleConfirmDialog mNoTitleConfirmDialog = new NoTitleConfirmDialog(QqIdUnLinkActivity.this,
+                        "确定解绑QQ",
+                        getString(R.string.ok), getString(R.string.cancel), getColor(R.color.navy_blue));
+                mNoTitleConfirmDialog.setOnDialogClickListener(new NoTitleConfirmDialog.OnDialogClickListener() {
+                    @Override
+                    public void onOkClick() {
+                        mNoTitleConfirmDialog.dismiss();
+                        mDialog.setMessage("解绑中");
+                        mDialog.setCanceledOnTouchOutside(false);
+                        mDialog.show();
+                        unLinkQqId(mUser.getUserId());
+                    }
+
+                    @Override
+                    public void onCancelClick() {
+                        mNoTitleConfirmDialog.dismiss();
+                    }
+                });
+                // 点击空白处消失
+                mNoTitleConfirmDialog.setCancelable(true);
+                mNoTitleConfirmDialog.show();
+            }
+        });
     }
 
     public void back(View view) {
         finish();
+    }
+
+    /**
+     * 解绑QQ号
+     *
+     * @param userId 用户ID
+     */
+    private void unLinkQqId(String userId) {
+        String url = Constant.BASE_URL + "users/" + userId + "/qqIdLink";
+
+        mVolleyUtil.httpDeleteRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                mDialog.dismiss();
+                // 解绑成功
+                mUser.setUserQqId("");
+                mUser.setUserQqPassword("");
+                mUser.setUserIsQqLinked(Constant.QQ_ID_NOT_LINK);
+                PreferencesUtil.getInstance().setUser(mUser);
+                QqIdUnLinkActivity.this.finish();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                mDialog.dismiss();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/bc/wechat/widget/NoTitleConfirmDialog.java
@@ -0,0 +1,149 @@
+package com.bc.wechat.widget;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.bc.wechat.R;
+
+/**
+ * 确认弹窗
+ *
+ * @author zhou
+ */
+public class NoTitleConfirmDialog extends Dialog {
+    private Context mContext;
+
+    private TextView mContentTv;
+    private Button mOkBtn;
+    private Button mCancelBtn;
+    private OnDialogClickListener mDialogClickListener;
+
+    // 内容
+    private String mContent;
+    private String mConfirm;
+    private String mCancel;
+
+    // 确认按钮颜色
+    private int mOkBtnColor = -1;
+
+    public NoTitleConfirmDialog(Context context, String content,
+                                String confirm, String cancel) {
+        super(context);
+        this.mContext = context;
+        this.mContent = content;
+        this.mConfirm = confirm;
+        this.mCancel = cancel;
+        initalize();
+    }
+
+    public NoTitleConfirmDialog(Context context, String content,
+                                String confirm, String cancel, int okBtnColor) {
+        super(context);
+        this.mContext = context;
+        this.mContent = content;
+        this.mConfirm = confirm;
+        this.mCancel = cancel;
+        this.mOkBtnColor = okBtnColor;
+        initalize();
+    }
+
+    // 初始化View
+    private void initalize() {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.dialog_confirm_no_title, null);
+        setContentView(view);
+        initWindow();
+
+        mContentTv = findViewById(R.id.tv_content);
+        mOkBtn = findViewById(R.id.btn_ok);
+        mCancelBtn = findViewById(R.id.btn_cancel);
+
+        if (!TextUtils.isEmpty(mContent)) {
+            mContentTv.setText(mContent);
+        }
+
+        if (!TextUtils.isEmpty(mConfirm)) {
+            mOkBtn.setText(mConfirm);
+        }
+
+        if (!TextUtils.isEmpty(mCancel)) {
+            mCancelBtn.setText(mCancel);
+        }
+
+        if (-1 != mOkBtnColor) {
+            mOkBtn.setTextColor(mOkBtnColor);
+        }
+
+
+        mOkBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dismiss();
+                if (mDialogClickListener != null) {
+                    mDialogClickListener.onOkClick();
+                }
+            }
+        });
+        mCancelBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dismiss();
+                if (mDialogClickListener != null) {
+                    mDialogClickListener.onCancelClick();
+                }
+            }
+        });
+    }
+
+    /**
+     * 添加黑色半透明背景
+     */
+    private void initWindow() {
+        Window dialogWindow = getWindow();
+        // 设置window背景
+        dialogWindow.setBackgroundDrawable(new ColorDrawable(0));
+        // 设置输入法显示模式
+        dialogWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN |
+                WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        WindowManager.LayoutParams layoutParams = dialogWindow.getAttributes();
+        // 获取屏幕尺寸
+        DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
+        // 宽度为屏幕80%
+        layoutParams.width = (int) (displayMetrics.widthPixels * 0.8);
+        // 中央居中
+        layoutParams.gravity = Gravity.CENTER;
+        dialogWindow.setAttributes(layoutParams);
+    }
+
+    public void setOnDialogClickListener(OnDialogClickListener clickListener) {
+        mDialogClickListener = clickListener;
+    }
+
+    /**
+     * 添加按钮点击事件
+     */
+    public interface OnDialogClickListener {
+        /**
+         * 确认
+         */
+        void onOkClick();
+
+        /**
+         * 取消
+         */
+        void onCancelClick();
+    }
+
+}
