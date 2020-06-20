ebf525add5a3cf0dc087d37f7a934f2eb163f864
BooksCup <812809680@qq.com>
2020-06-20 16:19:48 +0800

    渲染绑定手机号页面的数据

--- app/src/main/java/com/bc/wechat/activity/ModifyPhoneActivity.java
+++ app/src/main/java/com/bc/wechat/activity/ModifyPhoneActivity.java
@@ -2,8 +2,11 @@ package com.bc.wechat.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.bc.wechat.R;
+import com.bc.wechat.entity.User;
+import com.bc.wechat.utils.PreferencesUtil;
 
 /**
  * 修改手机号
@@ -11,11 +14,27 @@ import com.bc.wechat.R;
  * @author zhou
  */
 public class ModifyPhoneActivity extends BaseActivity {
+
+    private TextView mPhoneTv;
+    private User mUser;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_phone);
-
         initStatusBar();
+
+        mUser = PreferencesUtil.getInstance().getUser();
+
+        initView();
+    }
+
+    private void initView() {
+        mPhoneTv = findViewById(R.id.tv_phone);
+        mPhoneTv.setText("绑定的手机号：" + mUser.getUserPhone());
+    }
+
+    public void back() {
+        finish();
     }
 }
