7cab9a996b32eda2c177f14621d6a5b6022b71d5
BooksCup <812809680@qq.com>
2020-09-07 09:58:43 +0800

    temp commit

--- app/src/main/java/com/bc/wechat/activity/UserInfoActivity.java
+++ app/src/main/java/com/bc/wechat/activity/UserInfoActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -16,12 +18,14 @@ import com.bc.wechat.cons.Constant;
 import com.bc.wechat.dao.UserDao;
 import com.bc.wechat.entity.User;
 import com.bc.wechat.utils.CommonUtil;
+import com.bc.wechat.utils.DensityUtil;
 import com.bc.wechat.utils.PreferencesUtil;
 import com.bc.wechat.utils.StatusBarUtil;
 import com.bc.wechat.utils.VolleyUtil;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.Nullable;
@@ -61,11 +65,11 @@ public class UserInfoActivity extends BaseActivity {
     @BindView(R.id.tv_desc)
     TextView mDescTv;
 
-    @BindView(R.id.tv_phone_temp)
-    TextView mPhoneTempTv;
-
-    @BindView(R.id.tv_phone)
-    TextView mPhoneTv;
+//    @BindView(R.id.tv_mobile_temp)
+//    TextView mPhoneTempTv;
+//
+//    @BindView(R.id.tv_mobile)
+//    TextView mPhoneTv;
 
     @BindView(R.id.rl_edit_contact)
     RelativeLayout mEditContactRl;
@@ -73,8 +77,8 @@ public class UserInfoActivity extends BaseActivity {
     @BindView(R.id.rl_desc)
     RelativeLayout mDescRl;
 
-    @BindView(R.id.rl_phone)
-    RelativeLayout mPhoneRl;
+    @BindView(R.id.ll_mobile)
+    LinearLayout mMobileLl;
 
     // 星标好友
     @BindView(R.id.iv_star_friends)
@@ -149,15 +153,15 @@ public class UserInfoActivity extends BaseActivity {
             }
         });
 
-        mPhoneTempTv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(UserInfoActivity.this, EditContactActivity.class);
-                intent.putExtra("userId", contact.getUserId());
-                intent.putExtra("isFriend", contact.getIsFriend());
-                startActivity(intent);
-            }
-        });
+//        mPhoneTempTv.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(UserInfoActivity.this, EditContactActivity.class);
+//                intent.putExtra("userId", contact.getUserId());
+//                intent.putExtra("isFriend", contact.getIsFriend());
+//                startActivity(intent);
+//            }
+//        });
 
         mOperateRl.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -241,20 +245,14 @@ public class UserInfoActivity extends BaseActivity {
             mWxIdTv.setText("微信号：" + user.getUserWxId());
         }
 
-        // 电话号码
-        if (!TextUtils.isEmpty(user.getUserContactMobiles())) {
-            mPhoneRl.setVisibility(View.VISIBLE);
-            mPhoneTv.setText(user.getUserContactMobiles());
-        } else {
-            mPhoneRl.setVisibility(View.GONE);
-        }
-
-        // 描述
-        if (!TextUtils.isEmpty(user.getUserContactDesc())) {
-            mDescRl.setVisibility(View.VISIBLE);
-            mDescTv.setText(user.getUserContactDesc());
-        } else {
-            mDescRl.setVisibility(View.GONE);
+        List<String> mobileList;
+        try {
+            mobileList = JSON.parseArray(user.getUserContactMobiles(), String.class);
+            if (null == mobileList) {
+                mobileList = new ArrayList<>();
+            }
+        } catch (Exception e) {
+            mobileList = new ArrayList<>();
         }
 
         // 备注
@@ -267,10 +265,30 @@ public class UserInfoActivity extends BaseActivity {
             mNameTv.setText(user.getUserNickName());
         }
 
-        if (TextUtils.isEmpty(user.getUserContactDesc())) {
-            mEditContactRl.setVisibility(View.VISIBLE);
+        // 手机号
+        if (mobileList.size() > 0) {
+            mMobileLl.setVisibility(View.VISIBLE);
+            mMobileLl.removeAllViews();
+            for (int i = 0; i < mobileList.size(); i++) {
+                addMobileView(i, mobileList.size(), mobileList.get(i));
+            }
         } else {
+            mMobileLl.setVisibility(View.GONE);
+        }
+
+        // 描述
+        if (!TextUtils.isEmpty(user.getUserContactDesc())) {
+            mDescRl.setVisibility(View.VISIBLE);
+            mDescTv.setText(user.getUserContactDesc());
+        } else {
+            mDescRl.setVisibility(View.GONE);
+        }
+
+        // 电话号码,标签,描述任一信息不为空则隐藏"设置备注和标签"
+        if (!TextUtils.isEmpty(user.getUserContactDesc()) || mobileList.size() > 0) {
             mEditContactRl.setVisibility(View.GONE);
+        } else {
+            mEditContactRl.setVisibility(View.VISIBLE);
         }
 
         // 是否星标好友
@@ -287,12 +305,11 @@ public class UserInfoActivity extends BaseActivity {
      * @param userId 用户ID
      */
     public void getContactFromServer(final String userId, final String contactId) {
-        String url = Constant.BASE_URL + "users/" + userId + "/friends/" + contactId;
+        String url = Constant.BASE_URL + "users/" + userId + "/contacts/" + contactId;
 
         mVolleyUtil.httpGetRequest(url, response -> {
             User user = JSON.parseObject(response, User.class);
-
-//            mUserDao.saveUser(user);
+            mUserDao.saveUser(user);
             loadData(user);
         }, volleyError -> {
 
@@ -305,5 +322,32 @@ public class UserInfoActivity extends BaseActivity {
         User user = mUserDao.getUserById(userId);
         loadData(user);
         getContactFromServer(mUser.getUserId(), userId);
+    }
+
+    private void addMobileView(int index, int mobileListSize, String mobile) {
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                DensityUtil.dip2px(this, 48));
+        View view = LayoutInflater.from(this).inflate(R.layout.item_user_info_contact_mobile, null);
+        view.setLayoutParams(lp);
+        TextView mobileTempTv = view.findViewById(R.id.tv_mobile_temp);
+        View shortDividerView = view.findViewById(R.id.view_divider_short);
+        View longDividerView = view.findViewById(R.id.view_divider_long);
+        if (index == 0) {
+            mobileTempTv.setVisibility(View.VISIBLE);
+        } else {
+            mobileTempTv.setVisibility(View.INVISIBLE);
+        }
+
+        if (index == mobileListSize - 1) {
+            longDividerView.setVisibility(View.VISIBLE);
+            shortDividerView.setVisibility(View.GONE);
+        } else {
+            longDividerView.setVisibility(View.GONE);
+            shortDividerView.setVisibility(View.VISIBLE);
+        }
+
+        TextView mobileTv = view.findViewById(R.id.tv_mobile);
+        mobileTv.setText(mobile);
+        mMobileLl.addView(view);
     }
 }
