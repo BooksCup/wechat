47010c30032da4c5f0d90b958386ede12f2562de
BooksCup <812809680@qq.com>
2021-07-18 17:54:11 +0800

    优化绑定邮箱页面

--- app/src/main/java/com/bc/wechat/activity/EmailLinkActivity.java
+++ app/src/main/java/com/bc/wechat/activity/EmailLinkActivity.java
@@ -1,13 +1,10 @@
 package com.bc.wechat.activity;
 
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.bc.wechat.R;
 import com.bc.wechat.cons.Constant;
 import com.bc.wechat.entity.User;
@@ -19,50 +16,59 @@ import com.bc.wechat.widget.LoadingDialog;
 import java.util.HashMap;
 import java.util.Map;
 
-import androidx.annotation.Nullable;
-
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 绑定邮箱地址
  *
  * @author zhou
  */
-public class EmailLinkActivity extends BaseActivity2 implements View.OnClickListener {
+public class EmailLinkActivity extends BaseActivity {
 
-    private EditText mEmailEt;
-    private TextView mTipsTv;
-    private Button mUnLinkBtn;
-    private TextView mSaveTv;
+    @BindView(R.id.tv_title)
+    TextView mTitleTv;
 
-    private VolleyUtil mVolleyUtil;
-    private User mUser;
-    private LoadingDialog mDialog;
-    private boolean mIsEdit = true;
+    @BindView(R.id.et_email)
+    EditText mEmailEt;
+
+    @BindView(R.id.tv_tips)
+    TextView mTipsTv;
+
+    @BindView(R.id.btn_unlink)
+    Button mUnLinkBtn;
+
+    @BindView(R.id.tv_save)
+    TextView mSaveTv;
+
+    VolleyUtil mVolleyUtil;
+    User mUser;
+    LoadingDialog mDialog;
+    boolean mIsEdit = true;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_link_email);
+    public int getContentView() {
+        return R.layout.activity_link_email;
+    }
+
+    @Override
+    public void initView() {
         initStatusBar();
         StatusBarUtil.setStatusBarColor(EmailLinkActivity.this, R.color.bottom_text_color_normal);
+        setTitleStrokeWidth(mTitleTv);
+    }
 
+    @Override
+    public void initListener() {
+    }
+
+    @Override
+    public void initData() {
         mVolleyUtil = VolleyUtil.getInstance(this);
         mUser = PreferencesUtil.getInstance().getUser();
         mDialog = new LoadingDialog(this);
 
-        initView();
-    }
-
-    private void initView() {
-        mEmailEt = findViewById(R.id.et_email);
-        mTipsTv = findViewById(R.id.tv_tips);
-        mUnLinkBtn = findViewById(R.id.btn_unlink);
-        mSaveTv = findViewById(R.id.tv_save);
-
         mEmailEt.setText(mUser.getUserEmail());
-        mUnLinkBtn.setOnClickListener(this);
-        mSaveTv.setOnClickListener(this);
-
         String isEmailLinked = mUser.getUserIsEmailLinked();
         if (Constant.EMAIL_NOT_VERIFIED.equals(isEmailLinked)) {
             // 未验证
@@ -84,7 +90,7 @@ public class EmailLinkActivity extends BaseActivity2 implements View.OnClickList
         finish();
     }
 
-    @Override
+    @OnClick({R.id.btn_unlink, R.id.tv_save})
     public void onClick(View view) {
         String isEmailLinked = mUser.getUserIsEmailLinked();
         switch (view.getId()) {
@@ -135,26 +141,18 @@ public class EmailLinkActivity extends BaseActivity2 implements View.OnClickList
     private void unLinkEmail(String userId) {
         String url = Constant.BASE_URL + "users/" + userId + "/emailLink";
 
-        mVolleyUtil.httpDeleteRequest(url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String s) {
-                mDialog.dismiss();
-                mUser.setUserEmail("");
-                // 未绑定状态
-                mUser.setUserIsEmailLinked(Constant.EMAIL_NOT_LINK);
-                PreferencesUtil.getInstance().setUser(mUser);
-                mUnLinkBtn.setVisibility(View.GONE);
-                mSaveTv.setVisibility(View.VISIBLE);
+        mVolleyUtil.httpDeleteRequest(url, s -> {
+            mDialog.dismiss();
+            mUser.setUserEmail("");
+            // 未绑定状态
+            mUser.setUserIsEmailLinked(Constant.EMAIL_NOT_LINK);
+            PreferencesUtil.getInstance().setUser(mUser);
+            mUnLinkBtn.setVisibility(View.GONE);
+            mSaveTv.setVisibility(View.VISIBLE);
 
-                String content = "解绑完成后，下次登录请使用手机号" + mUser.getUserPhone() + "+微信独立密码 登录微信";
-                showNoTitleAlertDialog(EmailLinkActivity.this, content, "知道了");
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                mDialog.dismiss();
-            }
-        });
+            String content = "解绑完成后，下次登录请使用手机号" + mUser.getUserPhone() + "+微信独立密码 登录微信";
+            showNoTitleAlertDialog(EmailLinkActivity.this, content, "知道了");
+        }, volleyError -> mDialog.dismiss());
     }
 
     private void linkEmail(String userId, final String email, final String wechatId) {
@@ -169,27 +167,18 @@ public class EmailLinkActivity extends BaseActivity2 implements View.OnClickList
         paramMap.put("to", email);
         paramMap.put("wechatId", wechatId);
 
-        mVolleyUtil.httpPostRequest(url, paramMap, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String s) {
-                mDialog.dismiss();
-                mUser.setUserEmail(email);
-                // 邮箱已绑定但未验证
-                mUser.setUserIsEmailLinked(Constant.EMAIL_NOT_VERIFIED);
-                PreferencesUtil.getInstance().setUser(mUser);
+        mVolleyUtil.httpPostRequest(url, paramMap, s -> {
+            mDialog.dismiss();
+            mUser.setUserEmail(email);
+            // 邮箱已绑定但未验证
+            mUser.setUserIsEmailLinked(Constant.EMAIL_NOT_VERIFIED);
+            PreferencesUtil.getInstance().setUser(mUser);
 
-//                Toast.makeText(EmailLinkActivity.this, "验证邮件已发送，请尽快登录邮箱验证", Toast.LENGTH_SHORT).show();
+            showAlertDialog(EmailLinkActivity.this, "提示",
+                    "验证邮件已发送，请尽快登录邮箱验证",
+                    "确定", true);
 
-                showAlertDialog(EmailLinkActivity.this, "提示",
-                        "验证邮件已发送，请尽快登录邮箱验证",
-                        "确定", true);
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                mDialog.dismiss();
-            }
-        });
+        }, volleyError -> mDialog.dismiss());
     }
+
 }
